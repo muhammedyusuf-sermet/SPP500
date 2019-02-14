6f1892b4b822062797c78ddd8f71da88f11a92ff
Muhammed Ali Sit <masit@outlook.com>
2019-02-14 02:10:37 -0600

    changed model export

--- backend/model/User.js
+++ backend/model/User.js
@@ -1,14 +1,12 @@
-/*export */ class User {
-    constructor(id, name, username, email, password, type) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.type = type;
-    }
-}
-
 module.exports = {
-    User: User
+    User:  class User {
+	    constructor(id, name, username, email, password, type) {
+	        this.id = id;
+	        this.name = name;
+	        this.username = username;
+	        this.email = email;
+	        this.password = password;
+	        this.type = type;
+	    }
+	}
 };
