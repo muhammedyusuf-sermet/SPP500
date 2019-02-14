b7ddc52d31f332fdee1bc56bbe04c3c90a9ea3de
David McDermott <crazyfox343@gmail.com>
2019-02-13 23:10:50 -0600

    Make changing names easier.

--- backend/Database/PostgeSQL_Schema.sql
+++ backend/Database/PostgeSQL_Schema.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TYPE spell_components AS ENUM ('Verbal', 'Somatic', 'Material');
 
 CREATE TABLE "Spell" (
-        "Name" varchar(50),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
         "Level" integer DEFAULT 1,
         "Casting_Time" varchar(50) DEFAULT '1 action',
         "Range" varchar(50) DEFAULT '60 feet',
@@ -11,7 +12,6 @@ CREATE TABLE "Spell" (
         "Description" text ARRAY,
         "Higher_Level" text,
         "School" varchar(50),
-        PRIMARY KEY ("Name"),
         CONSTRAINT Positive_Level CHECK ("Level" > 0)
 );
 
@@ -23,55 +23,56 @@ CREATE TYPE tool_categories AS ENUM (
 );
 
 CREATE TABLE "Tool" (
-        "Name" varchar(50),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
         "Category" tool_categories,
         "Cost" integer DEFAULT 1,
         "Weight" integer DEFAULT 1,
         "Description" text,
-        PRIMARY KEY ("Name"),
         CONSTRAINT Positive_Cost CHECK ("Cost" > 0)
 );
 
 CREATE TABLE "Damage_Type" (
-        "Name" varchar(50),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
         "Description" varchar(500),
-        PRIMARY KEY ("Name")
 );
 
 CREATE TYPE weapon_types AS ENUM ('Melee', 'Ranged', 'Other');
 CREATE TYPE weapon_proficiencies AS ENUM ('Simple', 'Martial');
 
 CREATE TABLE "Weapon" (
-        "Name" varchar(50),
-        "Damage_Type_Name" varchar(50) REFERENCES "Damage_Type" ("Name"),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
+        "Damage_Type_Id" integer REFERENCES "Damage_Type" ("Id"),
         "Proficiency" weapon_proficiencies,
         "Type" weapon_types,
         "Cost" integer DEFAULT 1,
         "Damage" varchar(20) DEFAULT '1d4',
         "Range" varchar(20) DEFAULT '(5/-)',
         "Weight" integer DEFAULT 1,
-        PRIMARY KEY ("Name"),
         CONSTRAINT Positive_Cost CHECK ("Cost" > 0)
 );
 
 CREATE TABLE "Property" (
-        "Name" varchar(50),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
         "Description" varchar(500),
-        PRIMARY KEY ("Name")
 );
 
 CREATE TABLE "Weapon_Property" (
-        "Weapon_Name" varchar(50) REFERENCES "Weapon" ("Name"),
-        "Property_Name" varchar(50) REFERENCES "Property" ("Name"),
+        "Weapon_Id" integer REFERENCES "Weapon" ("Id"),
+        "Property_Id" integer REFERENCES "Property" ("Id"),
         "Damage_Modifier" varchar(20) DEFAULT '-',
         "Range_Modifier" varchar(20) DEFAULT '(-/-)',
-        PRIMARY KEY ("Weapon_Name", "Property_Name")
+        PRIMARY KEY ("Weapon_Id", "Property_Id")
 );
 
 CREATE TYPE armor_proficiencies AS ENUM ('Light', 'Medium', 'Heavy', 'Sheild');
 
 CREATE TABLE "Armor" (
-        "Name" varchar(50),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
         "Proficiency" armor_proficiencies,
         "Cost" integer DEFAULT 1,
         "Weight" integer DEFAULT 1,
@@ -80,7 +81,6 @@ CREATE TABLE "Armor" (
         "Max_Bonus" integer DEFAULT null,
         "Min_Strength" integer DEFAULT null,
         "Stealth_Disadvantage" boolean DEFAULT false,
-        PRIMARY KEY ("Name"),
         CONSTRAINT Positive_Armor_class CHECK ("Armor_Class" > 0),
         CONSTRAINT Positive_Cost CHECK ("Cost" > 0)
 );
@@ -95,48 +95,48 @@ CREATE TYPE adventuring_gear_categories AS ENUM (
 );
 
 CREATE TABLE "Adventuring_Gear" (
-        "Name" varchar(50),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
         "Category" adventuring_gear_categories,
         "Cost" integer DEFAULT 1,
         "Weight" integer DEFAULT 1,
         "Description" varchar(500),
-        PRIMARY KEY ("Name"),
         CONSTRAINT Positive_Cost CHECK ("Cost" > 0)
 );
 
 CREATE TABLE "Pack_Contents" (
-        "Equipment_Pack_Name" varchar(50) REFERENCES "Equipment_Pack" ("Name"),
-        "Adventuring_Gear_Name" varchar(50) REFERENCES "Adventuring_Gear" ("Name"),
+        "Equipment_Pack_Id" integer REFERENCES "Equipment_Pack" ("Id"),
+        "Adventuring_Gear_Id" integer REFERENCES "Adventuring_Gear" ("Id"),
         "Quantity" integer DEFAULT 1,
-        PRIMARY KEY ("Pack_Name", "Gear_Name"),
+        PRIMARY KEY ("Pack_Id", "Gear_Id"),
         CONSTRAINT Positive_Quantity CHECK ("Quantity" > 0)
 );
 
 CREATE TABLE "Equipment_Pack" (
-        "Name" varchar(50),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
         "Cost" integer DEFAULT 1,
-        PRIMARY KEY ("Name"),
         CONSTRAINT Positive_Cost CHECK ("Cost" > 0)
 );
 
 CREATE TABLE "Ability_Score" (
-        "Name" varchar(50),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
         "Abbreviation" char(3),
-        "Description" varchar(500),
-        PRIMARY KEY ("Name")
+        "Description" varchar(500)
 );
 
 CREATE TABLE "Skill" (
-        "Ability_Score_Name" varchar(50) REFERENCES "Ability_Score" ("Name"),
-        "Name" varchar(50),
-        "Description" varchar(500),
-        PRIMARY KEY ("Name")
+        "Ability_Score_Id" integer REFERENCES "Ability_Score" ("Id"),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
+        "Description" varchar(500)
 );
 
 CREATE TABLE "Condition" (
-        "Name" varchar(50),
-        "Description" varchar(500),
-        PRIMARY KEY ("Name")
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
+        "Description" varchar(500)
 );
 
 CREATE TYPE monster_size AS ENUM (
@@ -198,7 +198,8 @@ CREATE TYPE alignment AS ENUM (
 );
 
 CREATE TABLE "Monster" (
-        "Name" varchar(50),
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) UNIQUE NOT NULL,
         "Size" monster_size DEFAULT 'Medium',
         "Type" monster_type DEFAULT 'Beast',
         "Race" monster_race DEFAULT 'Any race',
@@ -215,7 +216,6 @@ CREATE TABLE "Monster" (
         "Actions" json DEFAULT '[]',
         "Reactions" json DEFAULT '[]',
         "Legendary_Actions" json DEFAULT '[]',
-        PRIMARY KEY ("Name"),
         CONSTRAINT Positive_Armor_class CHECK ("Armor_Class" > 0),
         CONSTRAINT Positive_Hit_Points CHECK ("Hit_Points" > 0),
         CONSTRAINT Positive_Challange_Rating CHECK ("Challange_Rating" > 0)
@@ -223,28 +223,28 @@ CREATE TABLE "Monster" (
 
 -- stats of the monsters ability scores
 CREATE TABLE "Monster_Ability_Score" (
-        "Monster_Name" varchar(50) REFERENCES "Monster" ("Name"),
-        "Damage_Type_Name" varchar(50) REFERENCES "Damage_Type" ("Name"),
+        "Monster_Id" integer REFERENCES "Monster" ("Id"),
+        "Damage_Type_Id" integer REFERENCES "Damage_Type" ("Id"),
         "Score" integer DEFAULT 0,
-        PRIMARY KEY ("Monster_Name", "Damage_Type_Name"),
+        PRIMARY KEY ("Monster_Id", "Damage_Type_Id"),
         CONSTRAINT Positive_Score CHECK ("Score" >= 0)
 )
 
 -- bonus applied when monster uses a specific skill
 CREATE TABLE "Monster_Skill" (
-        "Monster_Name" varchar(50) REFERENCES "Monster" ("Name"),
-        "Skill_Name" varchar(50) REFERENCES "Skill" ("Name"),
+        "Monster_Id" integer REFERENCES "Monster" ("Id"),
+        "Skill_Id" integer REFERENCES "Skill" ("Id"),
         "Bonus" integer DEFAULT 0,
-        PRIMARY KEY ("Monster_Name", "Skill_Name"),
+        PRIMARY KEY ("Monster_Id", "Skill_Id"),
         CONSTRAINT Positive_Bonus CHECK ("Bonus" >= 0),
 );
 
 -- bonus applied when monster has to do a saving throw for a specific ability
 CREATE TABLE "Monster_Saving_Throw" (
-        "Monster_Name" varchar(50) REFERENCES "Monster" ("Name"),
-        "Ability_Score_Name" varchar(50) REFERENCES "Ability_Score" ("Name"),
+        "Monster_Id" integer REFERENCES "Monster" ("Id"),
+        "Ability_Score_Id" integer REFERENCES "Ability_Score" ("Id"),
         "Bonus" integer DEFAULT 0,
-        PRIMARY KEY ("Monster_Name", "Ability_Score"),
+        PRIMARY KEY ("Monster_Id", "Ability_Score_Id"),
         CONSTRAINT Positive_Bonus CHECK ("Bonus" >= 0),
 );
 
@@ -257,15 +257,36 @@ CREATE TYPE resistance_type AS ENUM (
 
 -- types of damage that the monster is either vulnerable, resistant or immune to.
 CREATE TABLE "Monster_Damage_Type_Resistance" (
-        "Monster_Name" varchar(50) REFERENCES "Monster" ("Name"),
-        "Damage_Type_Name" varchar(50) REFERENCES "Damage_Type" ("Name"),
+        "Monster_Id" integer REFERENCES "Monster" ("Id"),
+        "Damage_Type_Id" integer REFERENCES "Damage_Type" ("Id"),
         "Type" resistance_type DEFAULT 'Resistance',
-        PRIMARY KEY ("Monster_Name", "Damage_Type_Name"),
+        PRIMARY KEY ("Monster_Id", "Damage_Type_Id"),
 );
 
 -- conditions that don't affect this monster
 CREATE TABLE "Monster_Condition_Immunity" (
-        "Monster_Name" varchar(50) REFERENCES "Monster" ("Name"),
-        "Condition_Name" varchar(50) REFERENCES "Condition" ("Name"),
-        PRIMARY KEY ("Monster_Name", "Condition_Name"),
+        "Monster_Id" integer REFERENCES "Monster" ("Id"),
+        "Condition_Id" integer REFERENCES "Condition" ("Id"),
+        PRIMARY KEY ("Monster_Id", "Condition_Id"),
+);
+
+CREATE TABLE "User" (
+        "Id" serial PRIMARY KEY,
+        "Name" varchar(50) NOT NULL,
+        "Email" varchar(200),
+        PRIMARY KEY ("Name")
+);
+
+CREATE TABLE "Encounter" (
+        "User_Id" varchar(50) REFERENCES "User" ("Id"),
+        "Id" serial,
+        "Name" varchar(50) NOT NULL,
+        PRIMARY KEY ("User_Id", "Id")
+);
+
+CREATE TABLE "Encounter_Location" (
+        "Encounter_Name" varchar(50), REFERENCES "Encounter" ("Name")
+        "Encounter_User_Name" varchar(50) REFERENCES "Encounter" ("User_Name")
+        "Location_Name" varchar(50) REFERENCES "Location"
+        PRIMARY KEY ("Name", "User_Name", "Location_Name")
 );
