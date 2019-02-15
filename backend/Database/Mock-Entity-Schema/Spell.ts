58eb84fbb783562fc7df6b30d92f7d3cd454a5ef
David McDermott <crazyfox343@gmail.com>
2019-02-15 02:14:19 -0600

    Completed Start of Spells Entity.

---
+++ backend/Database/Mock-Entity-Schema/Spell.ts
@@ -0,0 +1,77 @@
+import {Entity, Column, Index, PrimaryGeneratedColumn, Check} from "typeorm";
+
+export enum SpellComponents {
+    Verbal = "Verbal",
+    Somatic = "Somatic",
+    Material = "Material"
+}
+
+@Entity()
+export class Spell {
+
+    @PrimaryGeneratedColumn()
+    Id: number;
+
+    @Index()
+    @Column({
+        type: "varchar",
+        length: 50,
+    })
+    Name: string;
+
+    @Column({
+        type: "int",
+        default: 1
+    })
+    Level: number;
+
+    @Column({
+        type: "varchar",
+        length: 50,
+        default: "1 action"
+    })
+    CastingTime: string;
+
+    @Column({
+        type: "varchar",
+        length: 50,
+        default: "60 feet"
+    })
+    Range: string;
+
+    @Column({
+        type: "enum",
+        enum: SpellComponents,
+        array: true
+    })
+    Components: SpellComponents[];
+
+    @Column({
+        type: "varchar",
+        length: 250
+    })
+    Material: string;
+
+    @Column({
+        type: "varchar",
+        length: 50
+    })
+    Duration: string;
+
+    @Column({
+        type: "text",
+        array: true
+    })
+    Description: string[];
+
+    @Column({
+        type: "text"
+    })
+    HigherLevel: string;
+
+    @Column({
+        type: "varchar",
+        length: 50
+    })
+    School: string;
+}
