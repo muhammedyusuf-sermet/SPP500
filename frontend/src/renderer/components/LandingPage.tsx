1c2efc1a33a39be4aaea9c6e653d6b2e8ffe5784
Heather Kemp <heather-kemp@uiowa.edu>
2019-02-15 17:56:14 -0600

    Remove redundant name

--- frontend/src/renderer/components/LandingPage.tsx
+++ frontend/src/renderer/components/LandingPage.tsx
@@ -5,7 +5,6 @@ export const LandingPage: React.StatelessComponent<{}> = () => {
 	return (
 		<div className="landing-container">
 			<img src={require('../../../../doc/art/DM-Tools-Logo.png')} className="logo"></img>
-			<h1>DM Tools</h1>
 			<h3>One-stop platform for Dungeon Masters!</h3>
 		</div>
 	);
