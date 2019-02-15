1f248c8a5e97ed2aaec5a1577858ed043a4a0bc0
LeonGrund <15150811+LeonGrund@users.noreply.github.com>
2019-02-14 21:27:10 -0600

    parse Spells JSON

---
+++ backend/Database/Parse-Json.ts
@@ -0,0 +1,42 @@
+
+var fs = require('fs');
+let Spells: Array<any> = []
+
+fs.readFile('5e-SRD-Spells.json', ParseSpellData);
+
+function ParseSpellData (err: any, data: any): void{
+
+    let SpellData: any = JSON.parse(data)
+    
+    for (let i in SpellData)
+    {
+        let spell: any = 
+        {
+            Name: '',
+            Level: 1,
+            Casting_Time: '',
+            Range: '60 feet',
+            Components: [],
+            Material: '',
+            Duration: '',
+            Description: [],
+            Higher_Level: '',
+            School: ''
+        }
+
+        spell.Name = SpellData[i]['name']
+        spell.Level = SpellData[i]['level']
+        spell.Casting_Time = SpellData[i]['casting_time']
+        spell.Range = SpellData[i]['range']
+        spell.Components = SpellData[i]['components']
+        spell.Material = SpellData[i]['material']
+        spell.Duration = SpellData[i]['duration']
+        spell.Description = SpellData[i]['desc']
+        spell.Higher_Level = SpellData[i]['higher_level']
+        spell.School = SpellData[i]['school']['name']
+        
+        Spells.push(spell);
+    }
+    // Output all spells in console.
+    //console.log(Spells)
+}
