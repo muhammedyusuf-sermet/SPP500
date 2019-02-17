7b4a412c807e9049f51316fccdd5651148a87d88
Muhammed Ali Sit <masit@outlook.com>
2019-02-17 01:47:07 -0600

    psudo login tests

---
+++ backend/login.test.ts
@@ -0,0 +1,23 @@
+// START Login Tests
+
+// when a pair of username and the correct password 
+// for a registered user posted to the route
+// expect statuscode to be 201
+// expect a auth token
+
+// when a pair of email and the correct password 
+// for a registered user posted to the route
+// expect statuscode to be 201
+// expect a auth token
+
+// when a pair of email and a wrong password 
+// for a registered user posted to the route
+// expect statuscode to be 400
+// expect an error message
+
+// when a pair of email and a password that doesn't
+// match with any registered user posted to the route
+// expect statuscode to be 400
+// expect an error message
+
+ // END Login Tests
