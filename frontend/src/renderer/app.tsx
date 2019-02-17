e7867145937af7c7a3d3c17646bc7ca47dbe86cb
Heather Kemp <heather-kemp@uiowa.edu>
2019-02-17 17:46:44 -0600

    Restyle form with material

--- frontend/src/renderer/app.tsx
+++ frontend/src/renderer/app.tsx
@@ -7,42 +7,11 @@ import {MonsterCreation} from "./components/MonsterCreation";
 
 import './css/app.css';
 
-<<<<<<< HEAD
 export const App: React.StatelessComponent<{}> = () => {
   return (
     <div className="container-fluid">
       <div className="header"><LoginHeader/></div>
-      
-      <div className="horizontal-block">
-      	<div className="align-left"><LandingPage/></div>
-      	<div className="align-right"><Registration/></div>
         <div><MonsterCreation/></div>
       </div>
-    </div>
   );
 }
-=======
-type AppProps = {}
-
-export class App extends React.Component<{}> {
-	constructor(props: AppProps) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div className="container-fluid">
-				<div className="header"><LoginHeader/></div>
-			
-				<div className="horizontal-block">
-					<div className="align-left"><LandingPage/></div>
-					<div className="align-right"><Registration/></div>
-				</div>
-
-				<div className="footer"><Footer/></div>
-			</div>
-		);
-	}
-
-}
->>>>>>> master
--- frontend/src/renderer/components/MonsterCreation.tsx
+++ frontend/src/renderer/components/MonsterCreation.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+import "../css/create_monster.css"
 
 const validateForm = (event: React.FormEvent) => {
-	console.log("Hello!")
+	//console.log("Hello!")
 	event.preventDefault();
 }
 
 export const MonsterCreation: React.StatelessComponent<{}> = () => {
 	return (
-		<div className="form-container">
+		<div className="monster-creation-container">
 			<form onSubmit={validateForm} >
-				<h2>Sign up</h2>
+				<h1 className="page-title">Create a Monster</h1>
 				<div className="form-group">
-					<label htmlFor="password">Password</label>
-					<Input className="input" id="password" placeholder="Password" name="password" autoComplete="current-password" required/>
-					<label htmlFor="password">Password</label>
+					<TextField autoFocus margin="dense" id="name" label="Email Address" helperText="Test" fullWidth required/>
 				</div>
 				<Button className="button" variant="contained" color="primary" type="submit"> Create Monster </Button>
 			</form>
