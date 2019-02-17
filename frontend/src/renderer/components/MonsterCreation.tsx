afeaae03a11019ea20a97a0425366f170ee02619
Heather Kemp <heather-kemp@uiowa.edu>
2019-02-17 17:54:35 -0600

    Add form box with suffix unit

--- frontend/src/renderer/components/MonsterCreation.tsx
+++ frontend/src/renderer/components/MonsterCreation.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+
 
 import "../css/create_monster.css"
 
@@ -18,6 +22,13 @@ export const MonsterCreation: React.StatelessComponent<{}> = () => {
 				<div className="form-group">
 					<TextField autoFocus margin="dense" id="name" label="Email Address" helperText="Test" fullWidth required/>
 				</div>
+				<FormControl fullWidth>
+					<InputLabel htmlFor="adornment-amount">Amount</InputLabel>
+					<Input
+						id="adornment-amount"
+						endAdornment={<InputAdornment position="end">$</InputAdornment>}
+					/>
+				</FormControl>
 				<Button className="button" variant="contained" color="primary" type="submit"> Create Monster </Button>
 			</form>
 		</div>
