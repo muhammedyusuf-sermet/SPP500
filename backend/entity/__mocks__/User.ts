a089b3230038384a218ad29563392110020254cf
Muhammed Ali Sit <masit@outlook.com>
2019-02-17 18:11:42 -0600

    login tests

--- backend/entity/__mocks__/User.ts
+++ backend/entity/__mocks__/User.ts
@@ -19,6 +19,18 @@ export class User {
 		return result
 	}
 
+	static findOne(a) {
+		var result = User.users.slice(0);
+		for (let key in a) {
+			let value = a[key];
+			result = result.filter(function (el) {
+				return el[key] == value;
+			});
+		}
+
+		return result[0]
+	}
+
 	save() {
 		User.users.push({
 			Name: this.Name, 
--- backend/login.test.ts
+++ backend/login.test.ts
@@ -1,23 +1,129 @@
-// START Login Tests
+import {User} from "./entity/User";
+import {Registration} from "./registration";
+import {Login} from "./login";
 
-// when a pair of username and the correct password 
-// for a registered user posted to the route
-// expect statuscode to be 201
-// expect a auth token
+jest.mock("./entity/User");
 
-// when a pair of email and the correct password 
-// for a registered user posted to the route
-// expect statuscode to be 201
-// expect a auth token
 
-// when a pair of email and a wrong password 
-// for a registered user posted to the route
-// expect statuscode to be 400
-// expect an error message
+describe('login tests', async () => {
+	beforeAll( async () => {
+		var reg = new Registration();
+		const response = await reg.Register({
+			payload: {
+				"username": "john-doe",
+				"name": "John Doe",
+				"email": "john@doe.com",
+				"password": "testtest"
+			},
+		});
+	});
 
-// when a pair of email and a password that doesn't
-// match with any registered user posted to the route
-// expect statuscode to be 400
-// expect an error message
+	var log = new Login();
 
- // END Login Tests
+	test('when a pair of username and the correct password for a registered user posted to the route', async () => {
+		const response = await log.Login({
+			payload: {
+				"username": "john-doe",
+				"password": "testtest"
+			},
+		});
+
+		var payload = response['payload']
+
+		expect.assertions(3);
+		expect(response['status']).toBe(201);
+		expect(response['message']).toBe("Logged in successfully.");
+		expect(response['token']).toBeDefined();
+	});
+
+	test('when a pair of email and the correct password for a registered user posted to the route', async () => {
+		const response = await log.Login({
+			payload: {
+				"email": "john@doe.com",
+				"password": "testtest"
+			},
+		});
+
+		var payload = response['payload']
+
+		expect.assertions(3);
+		expect(response['status']).toBe(201);
+		expect(response['message']).toBe("Logged in successfully.");
+		expect(response['token']).toBeDefined();
+	});
+
+	test('when a pair of email and a wrong password for a registered user posted to the route', async () => {
+		const response = await log.Login({
+			payload: {
+				"email": "john@doe.com",
+				"password": "test"
+			},
+		});
+
+		var payload = response['payload']
+
+		expect.assertions(2);
+		expect(response['status']).toBe(400);
+		expect(response['message']).toBe("Given credentials do not match with our records.");
+	});
+
+	test('when a pair of username and a wrong password for a registered user posted to the route', async () => {
+		const response = await log.Login({
+			payload: {
+				"email": "john@doe.com",
+				"password": "test"
+			},
+		});
+
+		var payload = response['payload']
+
+		expect.assertions(2);
+		expect(response['status']).toBe(400);
+		expect(response['message']).toBe("Given credentials do not match with our records.");
+	});
+
+	test('when a pair of username and a wrong password for a registered user posted to the route', async () => {
+		const response = await log.Login({
+			payload: {
+				"username": "john-doe",
+				"password": "test"
+			},
+		});
+
+		var payload = response['payload']
+
+		expect.assertions(2);
+		expect(response['status']).toBe(400);
+		expect(response['message']).toBe("Given credentials do not match with our records.");
+	});
+
+	test('when a pair of email and a password that doesn not match with any registered user posted to the route', async () => {
+		const response = await log.Login({
+			payload: {
+				"username": "john",
+				"password": "test"
+			},
+		});
+
+		var payload = response['payload']
+
+		expect.assertions(2);
+		expect(response['status']).toBe(400);
+		expect(response['message']).toBe("Given credentials do not match with our records.");
+	});
+
+	test('when an unrelated request is made', async () => {
+		const response = await log.Login({
+			payload: {
+				"user": "john",
+				"password": "test"
+			},
+		});
+
+		var payload = response['payload']
+
+		expect.assertions(2);
+		expect(response['status']).toBe(400);
+		expect(response['message']).toBe("Bad request");
+	});
+});
