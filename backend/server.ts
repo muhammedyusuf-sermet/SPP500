2a56e779c76e640aaa44f898fe77627255b9a1d4
Muhammed Ali Sit <masit@outlook.com>
2019-02-17 18:13:24 -0600

    login functionality

--- backend/server.ts
+++ backend/server.ts
@@ -2,26 +2,75 @@
 
 import * as Hapi from "hapi";
 import * as Typeorm from "typeorm";
+import * as JWTHapi from "hapi-auth-jwt2";
 
 import {User} from "./entity/User";
 import {Registration} from "./registration";
+import {Login} from "./login";
+
 
 export const Server = new Hapi.Server({
 	port: 3000,
 	host: '0.0.0.0'
 });
 
-Server.route({
-	method: 'POST',
-	path: '/register',
-	handler: function (request, h) {
-		var reg = new Registration();
-		return reg.Register(request, h);
-	}
-});
+export const initServer = async () => {
+	await Server.register(JWTHapi);
 
+	Server.auth.strategy('jwt', 'jwt',
+		{
+			key: process.env.JWTSECRET,
+			validate: validate,
+			verifyOptions: { algorithms: [ 'HS256' ] }
+		}
+	);
+
+	Server.auth.default('jwt');
+
+	Server.route([{
+		method: 'POST',
+		path: '/register',
+		options: { auth: false },
+		handler: function (request) {
+			var reg = new Registration();
+			return reg.Register(request);
+		}
+	},
+	{
+		method: 'POST',
+		path: '/login',
+		options: { auth: false },
+		handler: function (request) {
+			var log = new Login();
+			return log.Login(request);
+		}
+	},
+	{
+		method: 'GET',
+		path: '/secret', 
+		options: { auth: 'jwt' },
+		handler: function (request) {
+			return {
+				"status": 201,
+				"message": "This is a secret content."
+			};
+		}
+	}]);
+}
+
+
+const validate = async function (decoded, request) {
+	var user = await User.findOne({ Id: decoded.id });
+
+	if (user) {
+		return { isValid: true };
+	} else {
+		return { isValid: false };
+	}
+};
 
 export const init = async () => {
+	await initServer();
 	await Server.start();
 	await Typeorm.createConnection({
 		"type": "postgres",
