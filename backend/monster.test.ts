16c9836b040773421f9d5b72685b49c62441e7f7
Muhammed Ali Sit <masit@outlook.com>
2019-02-24 11:25:15 -0600

    pseudo tests for monster creation

---
+++ backend/monster.test.ts
@@ -0,0 +1,15 @@
+/*
+
+When proper data is given to the API call along with a valid auth token
+It should create the monster and
+Return with a success message
+
+When malformed data is given to the API call along with a valid auth token
+It should not create the monster and
+Return with an error message saying that the problem is with the data
+
+When any data is given to the API call along with a non-valid auth token
+It should not create the monster and
+Return with an error message saying that there is an authentication problem
+
+*/
