527e5b3b93f830404f11f99f1c1cdfa08cf6e897
crazyfox44 <mcdermott.j.david@gmail.com>
2019-02-26 20:02:44 -0800

    Adding monster identifiers within the encounter.

--- backend/seeds/1551147888000-ParseEncounters.ts
+++ backend/seeds/1551147888000-ParseEncounters.ts
@@ -1,6 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 import { Encounter } from "../entity/Encounter";
 import Data from './data/encounters.json';
+import { Monster } from "../entity/Monster";
 
 export class ParseEncounters1551147888000 implements MigrationInterface {
 
@@ -8,7 +9,9 @@ export class ParseEncounters1551147888000 implements MigrationInterface {
         let encounters: Array<Encounter> = []
 
         for (let index in Data){
-            encounters.push(Data[index] as unknown as Encounter);
+            let encounter: Encounter = Data[index] as Encounter;
+            encounter.Monsters = Data[index]["Monsters"] as Monster[];
+            encounters.push(encounter);
             console.log(encounters[encounters.length - 1])
         }
 
@@ -18,6 +21,14 @@ export class ParseEncounters1551147888000 implements MigrationInterface {
             .into(Encounter)
             .values(encounters)
             .execute();
+        
+        for (let index in encounters){
+            await queryRunner.manager
+                .createQueryBuilder()
+                .relation(Encounter, "Monsters")
+                .of(encounters[index])
+                .add(encounters[index].Monsters)
+        }
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
