5782ff7afc904ae2ab9e6006f6d9df8a26c68794
Yusuf <muhammedyusuf-sermet@uiowa.edu>
2019-02-28 14:02:50 -0600

    Update Login and Register React Components with State
    The Footer and Landing Page components can be (might even better be) stateless as they are just static pages.

--- frontend/src/renderer/components/Registration.tsx
+++ frontend/src/renderer/components/Registration.tsx
@@ -6,71 +6,78 @@ import Button from '@material-ui/core/Button';
 
 import '../css/registration.css';
 
-export const Registration: React.StatelessComponent<{}> = () => {
-	return (
-		<div className="registration-container">
-			<React.Fragment>
-				<Typography variant="h6" gutterBottom>
-					Register Now!
-				</Typography>
-				<Grid container spacing={24}>
-					<Grid item xs={12} sm={6}>
-						<TextField
-							required
-							id="firstName"
-							name="firstName"
-							label="First name"
-							fullWidth
-							autoComplete="fname"
-						/>
+type AppProps = {}
+
+export class Registration extends React.Component<{}> {
+	constructor(props: AppProps) {
+		super(props);
+	}
+	render() {
+		return (
+			<div className="registration-container">
+				<React.Fragment>
+					<Typography variant="h6" gutterBottom>
+						Register Now!
+					</Typography>
+					<Grid container spacing={24}>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								id="firstName"
+								name="firstName"
+								label="First name"
+								fullWidth
+								autoComplete="fname"
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								id="lastName"
+								name="lastName"
+								label="Last name"
+								fullWidth
+								autoComplete="lname"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								id="email"
+								name="email"
+								label="E-mail Address"
+								type="email"
+								fullWidth
+								autoComplete="email"
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								id="username"
+								name="username"
+								label="Username"
+								fullWidth
+								autoComplete="username"
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField 
+								required
+								id="password"
+								name="password"
+								label="Password"
+								type="password"
+								fullWidth
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							{/*Needed for spacing purposes - No functionality*/}
+						</Grid>
 					</Grid>
-					<Grid item xs={12} sm={6}>
-						<TextField
-							required
-							id="lastName"
-							name="lastName"
-							label="Last name"
-							fullWidth
-							autoComplete="lname"
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<TextField
-							required
-							id="email"
-							name="email"
-							label="E-mail Address"
-							type="email"
-							fullWidth
-							autoComplete="email"
-						/>
-					</Grid>
-					<Grid item xs={12} sm={6}>
-						<TextField
-							required
-							id="username"
-							name="username"
-							label="Username"
-							fullWidth
-							autoComplete="username"
-						/>
-					</Grid>
-					<Grid item xs={12} sm={6}>
-						<TextField 
-							required
-							id="password"
-							name="password"
-							label="Password"
-							type="password"
-							fullWidth
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						{/*Needed for spacing purposes - No functionality*/}
-					</Grid>
-				</Grid>
-				<Button className="button" variant="contained" color="primary">Register</Button>
-			</React.Fragment>
-		</div>
-	);
+					<Button className="button" variant="contained" color="primary">Register</Button>
+				</React.Fragment>
+			</div>
+		);
+	}
 }
