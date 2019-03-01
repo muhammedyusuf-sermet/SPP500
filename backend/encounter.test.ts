224eb13c93fe6303fdd2d4e33f60486d8d0cc51c
Muhammed Ali Sit <masit@outlook.com>
2019-02-28 22:46:49 -0600

    encounter create pseudo tests

---
+++ backend/encounter.test.ts
@@ -0,0 +1,17 @@
+/*
+When proper data is given
+save the encounter
+
+When no monster is provided
+save the encounter
+
+When name is not provided
+raise an error with the status code of 400
+
+When description is not provided
+raise an error with the status code of 400
+
+When an invalid monster is provided
+raise an error with the status code of 400
+
+*/
