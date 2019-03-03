28b4b484795af49414f560169f13a55c5473b296
crazyfox44 <mcdermott.j.david@gmail.com>
2019-03-03 00:29:50 -0800

    Parse the actions into the database.

--- backend/seeds/2000000001500-ParseMonsters.ts
+++ backend/seeds/2000000001500-ParseMonsters.ts
@@ -1,56 +1,106 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 import Data from './data/monsters.json';
 import { Monster } from "../entity/Monster";
+import { MonsterSkill } from "../entity/MonsterSkill";
 import { MonsterAbilityScore } from "../entity/MonsterAbilityScore";
 import { MonsterSavingThrow } from "../entity/MonsterSavingThrow";
-import { MonsterSkill } from "../entity/MonsterSkill";
-import { Action } from "../entity/Action.js";
-import { MonsterAction } from "../entity/MonsterEnums.js";
+import { MonsterAction } from "../entity/MonsterEnums";
+import { Action } from "../entity/Action";
 
 export class ParseMonsters2000000001500 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        let monsters: Array<Monster> = []
-        
-        for (let index in Data){
-            const jsonMonster: any = Data[index];
-            const monster: Monster = jsonMonster as unknown as Monster;
-            monster.AbilityScores = jsonMonster["AbilityScores"] as unknown as MonsterAbilityScore;
-            monster.SavingThrows = jsonMonster["SavingThrows"] as unknown as MonsterSavingThrow;
-            monster.Skills = jsonMonster["Skills"] as unknown as MonsterSkill[];
+        let monsters: Array<Monster> = Data.map(value => {
+            const monster: Monster = Object.assign(new Monster(),value);
             monster.Actions = [];
-            let temp: Action[];
-            if(jsonMonster.hasOwnProperty("SpecialAbilities")){
-                temp = jsonMonster["SpecialAbilities"] as unknown as Action[];
-                for(var act of temp){
-                    act.Type = MonsterAction.SpecialAbility;
-                    monster.Actions.push(act);
+            if (value.Actions != undefined) {
+                for (let act of value.Actions) {
+                    const actv = act as {
+                        name: string,
+                        desc: string,
+                        attack_bonus: number,
+                        damage_dice?: string,
+                        damage_bonus?: number
+                    };
+                    const action:Action = new Action();
+                    action.Type = MonsterAction.Action;
+                    action.Name = actv.name;
+                    action.Description = actv.desc;
+                    action.HitBonus = actv.attack_bonus;
+                    if(actv.damage_dice){
+                        action.Damage = actv.damage_dice;
+                    }
+                    if(actv.damage_bonus) {
+                        action.DamageBonus = actv.damage_bonus;
+                    }
+                    action.Monster = monster;
+                    monster.Actions.push(action);
                 }
             }
-            if(jsonMonster.hasOwnProperty("Actions")){
-                temp = jsonMonster["Actions"] as unknown as Action[];
-                for(var act of temp){
-                    act.Type = MonsterAction.Action;
-                    monster.Actions.push(act);
+            if (value.SpecialAbilities != undefined) {
+                for (let act of value.SpecialAbilities) {
+                    const actv = act as {
+                        name: string,
+                        desc: string,
+                        attack_bonus: number,
+                        damage_dice?: string,
+                        damage_bonus?: number
+                    };
+                    const action:Action = new Action();
+                    action.Type = MonsterAction.SpecialAbility;
+                    action.Name = actv.name;
+                    action.Description = actv.desc;
+                    action.HitBonus = actv.attack_bonus;
+                    if(actv.damage_dice){
+                        action.Damage = actv.damage_dice;
+                    }
+                    if(actv.damage_bonus) {
+                        action.DamageBonus = actv.damage_bonus;
+                    }
+                    action.Monster = monster;
+                    monster.Actions.push(action);
                 }
             }
-            if(jsonMonster.hasOwnProperty("LegendaryActions")){
-                temp = jsonMonster["LegendaryActions"] as unknown as Action[];
-                for(var act of temp){
-                    act.Type = MonsterAction.LegendaryAction;
-                    monster.Actions.push(act);
+            if (value.LegendaryActions != undefined) {
+                for (let act of value.LegendaryActions) {
+                    const actv = act as {
+                        name: string,
+                        desc: string,
+                        attack_bonus: number,
+                        damage_dice?: string,
+                        damage_bonus?: number
+                    };
+                    const action:Action = new Action();
+                    action.Type = MonsterAction.LegendaryAction;
+                    action.Name = actv.name;
+                    action.Description = actv.desc;
+                    action.HitBonus = actv.attack_bonus;
+                    if(actv.damage_dice){
+                        action.Damage = actv.damage_dice;
+                    }
+                    if(actv.damage_bonus) {
+                        action.DamageBonus = actv.damage_bonus;
+                    }
+                    action.Monster = monster;
+                    monster.Actions.push(action);
                 }
             }
+            return monster;
+        });
+
+        for (let monster of monsters){
             await queryRunner.manager
                 .createQueryBuilder()
                 .insert()
                 .into(MonsterAbilityScore)
                 .values(monster.AbilityScores)
+                .execute();
+            await queryRunner.manager
+                .createQueryBuilder()
                 .insert()
                 .into(MonsterSavingThrow)
                 .values(monster.SavingThrows)
                 .execute();
-            monsters.push(monster);
         }
 
         await queryRunner.manager
@@ -69,13 +119,15 @@ export class ParseMonsters2000000001500 implements MigrationInterface {
                     .values(monster.Skills)
                     .execute();
             }
-            if (monster.Actions.length > 0){
-                await queryRunner.manager
-                    .createQueryBuilder()
-                    .insert()
-                    .into(Action)
-                    .values(monster.Actions)
-                    .execute();
+            if (monster.Actions){
+                if (monster.Actions.length > 0){
+                    await queryRunner.manager
+                        .createQueryBuilder()
+                        .insert()
+                        .into(Action)
+                        .values(monster.Actions)
+                        .execute();
+                }
             }
         }
     }
