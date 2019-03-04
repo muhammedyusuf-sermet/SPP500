36a716ade18ffcf96c328ecf1d4afb62d49f61bc
crazyfox44 <mcdermott.j.david@gmail.com>
2019-03-03 18:55:19 -0800

    Cleanup scraper to use interfaces and new scraped json.

--- backend/scrape/scrapeMonsters.ts
+++ backend/scrape/scrapeMonsters.ts
@@ -1,8 +1,12 @@
-import { MonsterAction } from "../entity/MonsterEnums";
+import { MonsterAction, Size, MonsterType, MonsterRace, Alignment, Environment } from "../entity/MonsterEnums";
+import { IMonsterData } from "../entity/Monster";
 
 var fs = require('fs');
 
-function ToUpperCamelCase (value: string): string{
+function ToUpperCamelCase (value: string): string | undefined{
+    if(value == undefined){
+        return undefined;
+    }
     var lowerCamel: string =  value.toLowerCase().replace(/[-\s]+(.)/g, function(match, group1) {
         return group1.toUpperCase();
     });
@@ -11,84 +15,34 @@ function ToUpperCamelCase (value: string): string{
 
 fs.readFile('5e-SRD-Monsters.json',(err: any, data: any) => {
 
-    interface IMonster {
-        Name: string,
-        Size?: string,
-        Type?: string,
-        Race?: string,       
-        Alignment?: string,
-        ArmorClass?: string,
-        HitPoints?: string,
-        HitPointDistribution?: string,
-        Speed?: string,
-        Senses?: string,
-        Languages?: string,
-        DamageVulnerabilities?: string,
-        DamageResistances?: string,
-        DamageImmunities?: string,
-        ConditionImmunities?: string,
-        ChallengeRating?: number,
-        AbilityScores: {
-            Strength?: string,
-            Dexterity?: string,
-            Constitution?: string,
-            Intelligence?: string,
-            Wisdom?: string,
-            Charisma?: string
-        },
-        Skills?: [],
-        SavingThrows: {
-            Strength?: string,
-            Dexterity?: string,
-            Constitution?: string,
-            Intelligence?: string,
-            Wisdom?: string,
-            Charisma?: string
-        },
-        Actions: {
-            Name: string,
-            Description?: string,
-            HitBonus?: number,
-            Damage?: string,
-            DamageBonus?: number,
-            Type: MonsterAction
-        }[]
-    };
-
-    var monsters: Array<IMonster> = []
+    var monsters: Array<IMonsterData> = []
     let Data: any = JSON.parse(data)
 
     for (let i in Data)
     {
-        let monster: IMonster =
-        {
+        let monster: IMonsterData = {
             Name: '',
-            Size: '',
-            Type: '',
-            Race: '',       
-            Alignment: '',
-            ArmorClass: '',
-            HitPoints: '',
-            HitPointDistribution: '',
-            Speed: '',
-            Senses: '',
-            Languages: '',
-            DamageVulnerabilities: '',
-            DamageResistances: '',
-            DamageImmunities: '',
-            ConditionImmunities: '',
-            ChallengeRating: 0,
             AbilityScores: {},
-            Skills: [],
             SavingThrows: {},
-            Actions: []
-        }
+            Skills: [],
+            Actions: [],
+            Encounters: []
+        };
 
-        monster.Name = Data[i]['name']
-        monster.Size = ToUpperCamelCase(Data[i]['size'])
-        monster.Type = ToUpperCamelCase(Data[i]['type'])
-        monster.Race = Data[i]['subtype'] == '' ? 'AnyRace' : ToUpperCamelCase(Data[i]['subtype'])
-        monster.Alignment = Data[i]['alignment'] == '' ? 'Unaligned' : ToUpperCamelCase(Data[i]['alignment'])
+        monster.Name = Data[i]['name'];
+        
+        const size = ToUpperCamelCase(Data[i]['size']);
+        const type = ToUpperCamelCase(Data[i]['type']);
+        const race = Data[i]['subtype'] == '' ? 'AnyRace' : ToUpperCamelCase(Data[i]['subtype']);
+        const alignment = Data[i]['alignment'] == '' ? 'Unaligned' : ToUpperCamelCase(Data[i]['alignment']);
+        const environment = Data[i]['environment'] ? Environment.Grassland : ToUpperCamelCase(Data[i]['environment']);
+
+        monster.Size = Size[size as keyof typeof Size];
+        monster.Type = MonsterType[type as keyof typeof MonsterType];
+        monster.Race = MonsterRace[race as keyof typeof MonsterRace];
+        monster.Alignment = Alignment[alignment as keyof typeof Alignment]
+        monster.Environment = Environment[environment as keyof typeof Environment]
+        
         monster.ArmorClass = Data[i]['armor_class']
         monster.HitPoints = Data[i]['hit_points']
         monster.HitPointDistribution = Data[i]['hit_dice']
@@ -102,33 +56,37 @@ fs.readFile('5e-SRD-Monsters.json',(err: any, data: any) => {
         monster.ChallengeRating = Data[i]['challenge_rating']
 
         // AbilityScores properties.
-        if (Data[i].hasOwnProperty('strength')) { monster.AbilityScores.Strength = Data[i]['strength'] }
-        if (Data[i].hasOwnProperty('dexterity')) { monster.AbilityScores.Dexterity = Data[i]['dexterity'] }
-        if (Data[i].hasOwnProperty('constitution')) { monster.AbilityScores.Constitution = Data[i]['constitution'] }
-        if (Data[i].hasOwnProperty('intelligence')) { monster.AbilityScores.Intelligence = Data[i]['intelligence'] }
-        if (Data[i].hasOwnProperty('wisdom')) { monster.AbilityScores.Wisdom = Data[i]['wisdom'] }
-        if (Data[i].hasOwnProperty('charisma')) { monster.AbilityScores.Charisma = Data[i]['charisma'] }
+        monster.AbilityScores = {
+            Strength: Data[i]['strength'],
+            Dexterity: Data[i]['dexterity'],
+            Constitution: Data[i]['constitution'],
+            Intelligence: Data[i]['intelligence'],
+            Wisdom: Data[i]['wisdom'],
+            Charisma: Data[i]['charisma']
+        };
 
         // Skills properties.
         // TODO: add skills.
 
         // SavingThrows properties.
-        if (Data[i].hasOwnProperty('strength_save')) { monster.SavingThrows.Strength = Data[i]['strength_save'] }
-        if (Data[i].hasOwnProperty('dexterity_save')) { monster.SavingThrows.Dexterity = Data[i]['dexterity_save'] }
-        if (Data[i].hasOwnProperty('constitution_save')) { monster.SavingThrows.Constitution = Data[i]['constitution_save'] }
-        if (Data[i].hasOwnProperty('intelligence_save')) { monster.SavingThrows.Intelligence = Data[i]['intelligence_save'] }
-        if (Data[i].hasOwnProperty('wisdom_save')) { monster.SavingThrows.Wisdom = Data[i]['wisdom_save'] }
-        if (Data[i].hasOwnProperty('charisma_save')) { monster.SavingThrows.Charisma = Data[i]['charisma_save'] }
+        monster.SavingThrows = {
+            Strength: Data[i]['strength_save'],
+            Dexterity: Data[i]['dexterity_save'],
+            Constitution: Data[i]['constitution_save'],
+            Intelligence: Data[i]['intelligence_save'],
+            Wisdom: Data[i]['wisdom_save'],
+            Charisma: Data[i]['charisma_save']
+        };
 
         monster.Actions = [];
         if(Data[i]['special_abilities']){
             Data[i]['special_abilities'].forEach((value: any) => {
                 monster.Actions.push({ 
-                    Name: value.name,
-                    Description: value.desc,
-                    HitBonus: value.attack_bonus,
-                    Damage: value.damage_dice,
-                    DamageBonus: value.damage_bonus,
+                    Name: value["name"],
+                    Description: value["desc"],
+                    HitBonus: value["attack_bonus"],
+                    Damage: value["damage_dice"],
+                    DamageBonus: value["damage_bonus"],
                     Type: MonsterAction.SpecialAbility
                 });
             });
@@ -137,10 +95,10 @@ fs.readFile('5e-SRD-Monsters.json',(err: any, data: any) => {
             Data[i]['actions'].forEach((value: any) => {
                 monster.Actions.push({ 
                     Name: value.name,
-                    Description: value.desc,
-                    HitBonus: value.attack_bonus,
-                    Damage: value.damage_dice,
-                    DamageBonus: value.damage_bonus,
+                    Description: value["desc"],
+                    HitBonus: value["attack_bonus"],
+                    Damage: value["damage_dice"],
+                    DamageBonus: value["damage_bonus"],
                     Type: MonsterAction.Action
                 });
             });
@@ -149,10 +107,10 @@ fs.readFile('5e-SRD-Monsters.json',(err: any, data: any) => {
             Data[i]['legendary_actions'].forEach((value: any) => {
                 monster.Actions.push({ 
                     Name: value.name,
-                    Description: value.desc,
-                    HitBonus: value.attack_bonus,
-                    Damage: value.damage_dice,
-                    DamageBonus: value.damage_bonus,
+                    Description: value["desc"],
+                    HitBonus: value["attack_bonus"],
+                    Damage: value["damage_dice"],
+                    DamageBonus: value["damage_bonus"],
                     Type: MonsterAction.LegendaryAction
                 });
             });
