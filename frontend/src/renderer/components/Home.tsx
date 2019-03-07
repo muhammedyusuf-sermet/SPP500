ec3e44aba484b915c4a5ffba69e5f73fb7164946
crazyfox44 <mcdermott.j.david@gmail.com>
2019-03-07 00:54:06 -0800

    NavBar contains login header.

--- frontend/src/renderer/components/Home.tsx
+++ frontend/src/renderer/components/Home.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {LoginHeader} from './LoginHeader';
 import {Footer} from './Footer';
 import {LandingPage} from './LandingPage';
 import {Registration} from './Registration';
@@ -9,11 +8,10 @@ import '../css/app.css';
 export const HomePage: React.StatelessComponent<{}> = () => {
 	return (
 		<div className="container-fluid">
-			<div className="header"><LoginHeader/></div>
-				<div className="horizontal-block">
-					<div className="align-left"><LandingPage/></div>
-					<div className="align-right"><Registration/></div>
-				</div>
+			<div className="horizontal-block">
+				<div className="align-left"><LandingPage/></div>
+				<div className="align-right"><Registration/></div>
+			</div>
 			<div className="footer"><Footer/></div>
 		</div>
 	);
