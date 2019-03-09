c1baab3af9f210cedf23ece2db6152038dc6c231
Heather Kemp <heather-kemp@uiowa.edu>
2019-03-08 18:32:06 -0600

    Updated cookie verification using api route

--- frontend/src/cookie.ts
+++ frontend/src/cookie.ts
@@ -1,21 +1,46 @@
-import Store from 'electron-store';
+import { API_URL } from "./config";
+
+const Store = require('electron-store');
+const store = new Store();
 
 export class CookieManager {
-	private constructor(){
+
+	public UserAuthenticated(cookieToCheck:string) {
+
+		var request = require("request");
+		var options = { method: 'POST',
+			url: API_URL + '/verify',
+			timeout: 2000,
+			headers:
+			{
+				'Cache-Control': 'no-cache',
+				'Content-Type': 'application/json',
+				'Authorization': cookieToCheck
+			},
+			body: {},
+			json: true
+		};
+
+		request(options, function (error: string, response: string, body: {status: number, message: string}) {
+			if (error) {
+				return false
+			}
+			else {
+				return (body.status === 201) // Success
+			}
+		});
+
+		return cookieToCheck !== undefined
 	}
 
-	private static store = new Store<string>();
-
-	public static UserToken(cookieToCheck:string):string {
-		// undefined if user is not logged in.
-		return CookieManager.store.get(cookieToCheck);
+	public SetStringCookie(data:string, name:string) {
+		store.set(name, data);
+	}
+	public GetCookie(name:string) {
+		return store.get(name);
 	}
 
-	public static SetStringCookie(name:string, data:string) {
-		CookieManager.store.set(name, data);
-	}
-
-	public static RemoveCookie(name:string) {
-		CookieManager.store.delete(name);
+	public RemoveCookie(name:string) {
+		store.delete(name);
 	}
 }
