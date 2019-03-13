1eb692650325836242b9e4a701d46ba0133c421c
LeonGrund <15150811+LeonGrund@users.noreply.github.com>
2019-03-13 09:37:29 -0600

    Added migration to parse Skills

---
+++ backend/seeds/2000000003000-ParseSkills.ts
@@ -0,0 +1,36 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+import { Skill } from "../entity/Skill";
+import Data from './data/skills.json';
+
+export class ParseSkills2000000003000 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        let skills: Array<Skill> = []
+
+        for (let index in Data){
+            skills.push(Data[index] as Skill);
+        }
+
+        await queryRunner.manager
+            .createQueryBuilder()
+            .insert()
+            .into(Skill)
+            .values(skills)
+            .execute();
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        let skills: Array<Skill> = []
+
+        for (let index in Data){
+            skills.push(Data[index] as Skill);
+        }
+
+        await queryRunner.manager
+            .createQueryBuilder()
+            .delete()
+            .from(Skill)
+            .where(skills)
+            .execute();
+    }
+}
