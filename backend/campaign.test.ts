1edf38bb469cbe42d4a999cd5b89d71da0813abc
Muhammed Ali Sit <masit@outlook.com>
2019-03-15 17:56:47 -0500

    campaign edit tests

--- backend/campaign.test.ts
+++ backend/campaign.test.ts
@@ -1,3 +1,204 @@
+import {Encounter} from "./entity/Encounter"
+import {User} from "./entity/User"
+import {Campaign} from "./entity/Campaign"
+
+import {CampaignFactory} from "./campaign";
+
+jest.mock("./entity/Campaign");
+jest.mock("./entity/Encounter");
+jest.mock("./entity/User");
+
+describe('campaign edit tests', async () => {
+	beforeAll( async () => {
+		const encounter = new Encounter();
+		encounter.Name = "Test1";
+		encounter.Id = 1;
+		await encounter.save();
+
+		const encounter2 = new Encounter();
+		encounter2.Name = "Test2";
+		encounter2.Id = 2;
+		await encounter2.save();
+
+		const user = new User();
+		user.Name = "John Doe";
+		user.Id = 1;
+		await user.save();
+
+		const campaign = new Campaign();
+		campaign.Name = "Test1";
+		campaign.Id = 1;
+		campaign.Creator = user;
+		campaign.Encounters = [encounter];
+		await campaign.save();
+
+	});
+
+	var campaignFactory = new CampaignFactory();
+
+	test('When name is provided to change', async () => {
+		const response = await campaignFactory.Edit({
+			payload: {
+				"Id": 1,
+				"Name": "new name",
+				"Summary": "same summary",
+				"Notes": "same notes"
+			},
+			auth: {
+				credentials: {
+					id: 1
+				}
+			}
+		});
+
+		expect.assertions(3);
+
+		expect(response['status']).toBe(201);
+		expect(response['messages'].length).toBe(1)
+		expect(response['messages'][0]).toBe("success");
+	});
+
+	test('When provided name is empty', async () => {
+		const response = await campaignFactory.Edit({
+			payload: {
+				"Id": 1,
+				"Name": "",
+				"Summary": "same summary"
+			},
+			auth: {
+				credentials: {
+					id: 1
+				}
+			}
+		});
+
+
+		expect.assertions(3);
+
+		expect(response['status']).toBe(400);
+		expect(response['messages'].length).toBe(1)
+		expect(response['messages'][0]).toBe("Name should not be an empty string.");
+	});
+
+	test('When provided summary is empty', async () => {
+		const response = await campaignFactory.Edit({
+			payload: {
+				"Id": 1,
+				"Name": "same name",
+				"Summary": ""
+			},
+			auth: {
+				credentials: {
+					id: 1
+				}
+			}
+		});
+
+
+		expect.assertions(3);
+		expect(response['status']).toBe(400);
+		expect(response['messages'].length).toBe(1)
+		expect(response['messages'][0]).toBe("Summary should not be an empty string.");
+	});
+
+	test('When a new set of valid encounters is provided to change', async () => {
+		const response = await campaignFactory.Edit({
+			payload: {
+				"Id": 1,
+				"Name": "same name",
+				"Summary": "same summary",
+				"Encounters": [
+					{"Id": 2}
+				]
+			},
+			auth: {
+				credentials: {
+					id: 1
+				}
+			}
+		});
+
+		expect.assertions(3);
+
+		expect(response['status']).toBe(201);
+		expect(response['messages'].length).toBe(1)
+		expect(response['messages'][0]).toBe("success");
+	});
+
+	test('When some encounters are invalid within a given set of encounters', async () => {
+		const response = await campaignFactory.Edit({
+			payload: {
+				"Id": 1,
+				"Name": "same name",
+				"Summary": "same summary",
+				"Encounters": [
+					{"Id": 3}
+				]
+			},
+			auth: {
+				credentials: {
+					id: 1
+				}
+			}
+		});
+	expect.assertions(3);
+
+		expect(response['status']).toBe(400);
+		expect(response['messages'].length).toBe(1)
+		expect(response['messages'][0]).toBe("Encounter is invalid: 3");
+	});
+
+	test('When requester is not the creator of the campaign', async () => {
+		const response = await campaignFactory.Edit({
+			payload: {
+				"Id": 1,
+				"Name": "same name",
+				"Summary": "same summary",
+				"Encounters": [
+					{"Id": 2}
+				]
+			},
+			auth: {
+				credentials: {
+					id: 2
+		}
+			}
+		});
+
+		expect.assertions(3);
+
+		expect(response['status']).toBe(400);
+		expect(response['messages'].length).toBe(1)
+		expect(response['messages'][0]).toBe("Requester is not the creator of this campaign.");
+	});
+
+	test('When no campaign with given id', async () => {
+		const response = await campaignFactory.Edit({
+			payload: {
+				"Id": 2,
+				"Name": "same name",
+				"Summary": "same summary",
+				"Encounters": [
+					{"Id": 2}
+				]
+			},
+			auth: {
+				credentials: {
+					id: 1
+				}
+			}
+		});
+
+
+		expect.assertions(3);
+
+		expect(response['status']).toBe(400);
+		expect(response['messages'].length).toBe(1)
+		expect(response['messages'][0]).toBe("There is no such campaign saved.");
+	});
+
+});
+
 /*
 Edit campaign tests
 
--- backend/entity/__mocks__/Campaign.ts
+++ backend/entity/__mocks__/Campaign.ts
@@ -1,30 +1,31 @@
-import { Monster } from "./Monster";
+import { Encounter } from "./Encounter";
 import { User } from "./User";
 
-export class Encounter {
+export class Campaign {
     Creator: User;
     Id: number;
     Name: string;
-    Description: string;
-    Monsters: Monster[];
+    Summary: string;
+    Notes: string;
+    Encounters: Encounter[];
 
-    [key: string]: any|number|string|User|Monster[]|(()=>void);
+    [key: string]: any|number|string|User|Encounter[]|(()=>void);
 
-    static TableRows: Encounter[] = [];
+    static TableRows: Campaign[] = [];
 
     static find(a: any) {
-        var result = Encounter.TableRows.slice(0);
+        var result = Campaign.TableRows.slice(0);
         for (let key in a) {
             let value = a[key];
             if (typeof value == 'object') {
                 for (let key2 in value) {
                     let value2 = value[key2]
-                    result = result.filter(function (el: Encounter) {
+                    result = result.filter(function (el: Campaign) {
                         return el[key][key2] == value2;
                     });
                 }
             } else {
-                result = result.filter(function (el: Encounter) {
+                result = result.filter(function (el: Campaign) {
                     return el[key] == value;
                 });
             }
@@ -38,13 +39,13 @@ export class Encounter {
     }
 
     remove() {
-        var index = Encounter.TableRows.indexOf(this);
+        var index = Campaign.TableRows.indexOf(this);
         if (index !== -1) {
-            Encounter.TableRows.splice(index, 1);
+            Campaign.TableRows.splice(index, 1);
         } 
     }
 
     save() {
-        Encounter.TableRows.push(this)
+        Campaign.TableRows.push(this)
     }
 }
