001d12f5e8c01eece660a281b1f4a82d57d49262
crazyfox44 <mcdermott.j.david@gmail.com>
2019-03-15 01:25:15 -0700

    get all skills, map name to skill, then add bonus.

--- backend/seeds/2000000001500-ParseMonsters.ts
+++ backend/seeds/2000000001500-ParseMonsters.ts
@@ -5,18 +5,42 @@ import { MonsterSkill } from "../entity/MonsterSkill";
 import { MonsterAbilityScore } from "../entity/MonsterAbilityScore";
 import { MonsterSavingThrow } from "../entity/MonsterSavingThrow";
 import { Action } from "../entity/Action";
+import { Skill } from "../entity/Skill";
 
 export class ParseMonsters2000000001500 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
+        
+        const allSkills: Skill[] = await queryRunner.manager
+            .createQueryBuilder(Skill, "skill") 
+            .select("skill")
+            .getMany();
+
+        const skillLookup: Map<string,Skill> = new Map();
+        allSkills.forEach((value: Skill) => {
+            skillLookup.set(value.Name , value)
+        });
+
         let monsters: Array<Monster> = Data.map(value => {
             const monster: Monster = Object.assign(new Monster(),value);
             monster.Actions = [];
-            if (value.Actions != undefined) {
+            if (value.Actions) {
                 for (let act of value.Actions) {
                     const action:Action = Object.assign(new Action(), act);
                     action.Monster = monster;
                     monster.Actions.push(action);
+                }
+            }
+            monster.Skills = []
+            if (value.Skills) {
+                for (let skill of value.Skills) {
+                    const monsterSkill: MonsterSkill = new MonsterSkill();
+                    monsterSkill.Monster = monster;
+                    const skillObj = skillLookup.get(skill.Name);
+                    if (skillObj)
+                        monsterSkill.Skill = skillObj;
+                    monsterSkill.Bonus = skill.Bonus;
+                    monster.Skills.push(monsterSkill);
                 }
             }
             return monster;
