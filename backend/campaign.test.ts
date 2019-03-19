4d85e22e48b7cc345704269f59fc2d1a6e5f464f
crazyfox44 <mcdermott.j.david@gmail.com>
2019-03-18 20:27:25 -0700

    Static array remains across each test.

--- backend/campaign.test.ts
+++ backend/campaign.test.ts
@@ -8,213 +8,25 @@ jest.mock("./entity/Campaign");
 jest.mock("./entity/Encounter");
 jest.mock("./entity/User");
 
-
-describe('campaign edit tests', async () => {
+describe('campaign tests', async () => {
 	beforeAll( async () => {
-		const encounter = new Encounter();
-		encounter.Name = "Test1";
-		encounter.Id = 1;
-		await encounter.save();
-
-		const encounter2 = new Encounter();
-		encounter2.Name = "Test2";
-		encounter2.Id = 2;
-		await encounter2.save();
-
-		const user = new User();
+		let user = new User();
 		user.Name = "John Doe";
 		user.Id = 1;
 		await user.save();
-    
-    	const campaign = new Campaign();
-		campaign.Name = "Test1";
-		campaign.Id = 1;
-		campaign.Creator = user;
-		campaign.Encounters = [encounter];
-		await campaign.save();
-	});
-
-	var campaignFactory = new CampaignFactory();
-
-	test('When name is provided to change', async () => {
-		const response = await campaignFactory.Edit({
-			payload: {
-				"Id": 1,
-				"Name": "new name",
-				"Summary": "same summary",
-				"Notes": "same notes"
-        },
-			auth: {
-				credentials: {
-					id: 1
-				}
-			}
-		});
-
-		expect.assertions(3);
-
-		expect(response['status']).toBe(201);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("success");
-	});
-  	test('When provided name is empty', async () => {
-		const response = await campaignFactory.Edit({
-			payload: {
-				"Id": 1,
-				"Name": "",
-				"Summary": "same summary"
-        	},
-			auth: {
-				credentials: {
-					id: 1
-				}
-			}
-		});
-
-		expect.assertions(3);
-		expect(response['status']).toBe(400);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("Name should not be an empty string.");
-	});
-
-	test('When provided summary is empty', async () => {
-		const response = await campaignFactory.Edit({
-			payload: {
-				"Id": 1,
-				"Name": "same name",
-				"Summary": ""
-        	},
-			auth: {
-				credentials: {
-					id: 1
-				}
-			}
-		});
-    
-		expect.assertions(3);
-		expect(response['status']).toBe(400);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("Summary should not be an empty string.");
-	});
-
-	test('When a new set of valid encounters is provided to change', async () => {
-		const response = await campaignFactory.Edit({
-			payload: {
-				"Id": 1,
-				"Name": "same name",
-				"Summary": "same summary",
-				"Encounters": [
-					{"Id": 2}
-				]
-        	},
-			auth: {
-				credentials: {
-					id: 1
-				}
-			}
-		});
-
-		expect.assertions(3);
-    	expect(response['status']).toBe(201);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("success");
-	});
-
-	test('When some encounters are invalid within a given set of encounters', async () => {
-		const response = await campaignFactory.Edit({
-			payload: {
-				"Id": 1,
-				"Name": "same name",
-				"Summary": "same summary",
-				"Encounters": [
-					{"Id": 3}
-          		]
-			},
-			auth: {
-				credentials: {
-					id: 1
-				}
-			}
-		});
-
-    	expect.assertions(3);
-		expect(response['status']).toBe(400);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("Encounter is invalid: 3");
-	});
-
-	test('When requester is not the creator of the campaign', async () => {
-		const response = await campaignFactory.Edit({
-			payload: {
-				"Id": 1,
-				"Name": "same name",
-				"Summary": "same summary",
-				"Encounters": [
-					{"Id": 2}
-          		]
-			},
-			auth: {
-				credentials: {
-         			id: 2
-				}
-        	}
-		});
-
-		expect.assertions(3);
-		expect(response['status']).toBe(400);
-		expect(response['messages'].length).toBe(1)
-    	expect(response['messages'][0]).toBe("Requester is not the creator of this campaign.");
-	});
-
-	test('When no campaign with given id', async () => {
-		const response = await campaignFactory.Edit({
-			payload: {
-				"Id": 2,
-				"Name": "same name",
-				"Summary": "same summary",
-        		"Encounters": [
-					{"Id": 2}
-				]
-			},
-			auth: {
-				credentials: {
-          			id: 1
-          		}
-			}
-		});
-    
-		expect.assertions(3);
-		expect(response['status']).toBe(400);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("There is no such campaign saved.");
-	});
-
-});
-
-/*
-Edit campaign tests
-
-When data is valid
-Return success
-
-When invalid encounter is added
-Return error
-
-When unauthorized user requests change
-Return error
-*/
-describe('campaign creation tests', async () => {
-	beforeAll( async () => {
-		let user = new User();
-    	user.Name = "John Doe";
-		user.Id = 1;
-		await user.save();
-    
+	
 		let encounter = new Encounter();
 		encounter.Name = "John Doe's Encounter";
 		encounter.Id = 1;
 		encounter.Creator = user;
 		await encounter.save();
+
+		const campaign = new Campaign();
+		campaign.Name = "Test1";
+		campaign.Id = 1;
+		campaign.Creator = user;
+		campaign.Encounters = [encounter];
+		await campaign.save();
 
 		user = new User();
 		user.Name = "Other John Doe";
@@ -226,173 +38,347 @@ describe('campaign creation tests', async () => {
 		encounter.Id = 2;
 		encounter.Creator = user;
 		await encounter.save();
+    
+    	
 	});
+	describe('campaign edit tests', async () => {
 
-	var campaign = new CampaignFactory();
+		var campaignFactory = new CampaignFactory();
 
-	test('when proper data is given', async () => {
-		const response = await campaign.Create({
-			payload: {
-				"Name": "Test",
-				"Summary": "Test",
-				"Encounters": [
-					{"Id": 1}
-				]
-        	},
-			auth: {
-				credentials: {
-					id: 1
+		test('When name is provided to change', async () => {
+			const response = await campaignFactory.Edit({
+				payload: {
+					"Id": 1,
+					"Name": "new name",
+					"Summary": "same summary",
+					"Notes": "same notes"
+	        },
+				auth: {
+					credentials: {
+						id: 1
+					}
 				}
-			}
+			});
+
+			expect.assertions(3);
+
+			expect(response['status']).toBe(201);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("success");
+		});
+	  	test('When provided name is empty', async () => {
+			const response = await campaignFactory.Edit({
+				payload: {
+					"Id": 1,
+					"Name": "",
+					"Summary": "same summary"
+	        	},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("Name should not be an empty string.");
 		});
 
-		expect.assertions(3);
-		expect(response['status']).toBe(201);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("success");
-	});
-	test('when no encounter is provided', async () => {
-		const response = await campaign.Create({
-			payload: {
-				"Name": "Test",
-				"Summary": "Test",
-				"Notes": "Test"
-        	},
-			auth: {
-				credentials: {
-					id: 1
+		test('When provided summary is empty', async () => {
+			const response = await campaignFactory.Edit({
+				payload: {
+					"Id": 1,
+					"Name": "same name",
+					"Summary": ""
+	        	},
+				auth: {
+					credentials: {
+						id: 1
+					}
 				}
-			}
-		});
-
-		expect.assertions(3);
-		expect(response['status']).toBe(201);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("success");
-	});
-
-	test('when name is not provided', async () => {
-		const response = await campaign.Create({
-			payload: {
-				"Summary": "Test"
-			},
-			auth: {
-				credentials: {
-					id: 1
-				}
-			}
-		});
-
-    	expect.assertions(3);
-		expect(response['status']).toBe(400);
-		expect(response['messages'].length).toBe(1)
-    	expect(response['messages'][0]).toBe("\"Name\" is required");
-	});
-
-	test('when summary is not provided', async () => {
-		const response = await campaign.Create({
-			payload: {
-				"Name": "Test"
-        	},
-			auth: {
-				credentials: {
-					id: 1
-				}
-			}
-		});
+			});
 		
-		expect.assertions(3);
-		expect(response['status']).toBe(400);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("\"Summary\" is required");
-	});
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("Summary should not be an empty string.");
+		});
 
-	test('when an empty string is provided as the name', async () => {
-		const response = await campaign.Create({
-			payload: {
-				"Name": "",
-				"Summary": "Test"
-        	},
-			auth: {
-				credentials: {
-					id: 1
+		test('When a new set of valid encounters is provided to change', async () => {
+			const response = await campaignFactory.Edit({
+				payload: {
+					"Id": 1,
+					"Name": "same name",
+					"Summary": "same summary",
+					"Encounters": [
+						{"Id": 2}
+					]
+	        	},
+				auth: {
+					credentials: {
+						id: 1
+					}
 				}
-			}
+			});
+
+			expect.assertions(3);
+	    	expect(response['status']).toBe(201);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("success");
 		});
 
-		expect.assertions(3);
-		expect(response['status']).toBe(400);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("\"Name\" is not allowed to be empty");
-	});
-
-	test('when credentials dont match the encounter', async () => {
-		const response = await campaign.Create({
-			payload: {
-				"Name": "Test",
-				"Summary": "Test",
-				"Encounters": [
-					{"Id": 2}
-          		]
-			},
-			auth: {
-				credentials: {
-					id: 1
+		test('When some encounters are invalid within a given set of encounters', async () => {
+			const response = await campaignFactory.Edit({
+				payload: {
+					"Id": 1,
+					"Name": "same name",
+					"Summary": "same summary",
+					"Encounters": [
+						{"Id": 3}
+	          		]
+				},
+				auth: {
+					credentials: {
+						id: 1
+					}
 				}
-			}
+			});
+
+	    	expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("Encounter is invalid: 3");
 		});
 
-		expect.assertions(3);
-		expect(response['status']).toBe(400);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("\"Encounter Id\" 2 is invalid");
-	});
+		test('When requester is not the creator of the campaign', async () => {
+			const response = await campaignFactory.Edit({
+				payload: {
+					"Id": 1,
+					"Name": "same name",
+					"Summary": "same summary",
+					"Encounters": [
+						{"Id": 2}
+	          		]
+				},
+				auth: {
+					credentials: {
+	         			id: 2
+					}
+	        	}
+			});
 
-	test('when invalid encounter is given', async () => {
-		const response = await campaign.Create({
-			payload: {
-				"Name": "Test",
-				"Summary": "Test",
-				"Encounters": [
-					{"Id": 3}
-          		]
-			},
-			auth: {
-				credentials: {
-					id: 1
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+	    	expect(response['messages'][0]).toBe("Requester is not the creator of this campaign.");
+		});
+
+		test('When no campaign with given id', async () => {
+			const response = await campaignFactory.Edit({
+				payload: {
+					"Id": 2,
+					"Name": "same name",
+					"Summary": "same summary",
+	        		"Encounters": [
+						{"Id": 2}
+					]
+				},
+				auth: {
+					credentials: {
+	          			id: 1
+	          		}
 				}
-        	}
-		});
-
-		expect.assertions(3);
-		expect(response['status']).toBe(400);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("\"Encounter Id\" 3 is invalid");
-	});
-
-	test('when credentials match the encounter', async () => {
-		const response = await campaign.Create({
-			payload: {
-				"Name": "Test",
-				"Summary": "Test",
-        		"Encounters": [
-					{"Id": 2}
-				]
-			},
-			auth: {
-				credentials: {
-					id: 2
-          		}
-			}
-		});
+			});
 		
-		expect.assertions(3);
-		expect(response['status']).toBe(201);
-		expect(response['messages'].length).toBe(1)
-		expect(response['messages'][0]).toBe("success");
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("There is no such campaign saved.");
+		});
+
+	});
+
+	/*
+	Edit campaign tests
+
+	When data is valid
+	Return success
+
+	When invalid encounter is added
+	Return error
+
+	When unauthorized user requests change
+	Return error
+	*/
+	describe('campaign creation tests', async () => {
+
+		var campaign = new CampaignFactory();
+
+		test('when proper data is given', async () => {
+			const response = await campaign.Create({
+				payload: {
+					"Name": "Test",
+					"Summary": "Test",
+					"Encounters": [
+						{ "Id": 1 }
+					]
+	        	},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(201);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("success");
+		});
+		test('when no encounter is provided', async () => {
+			const response = await campaign.Create({
+				payload: {
+					"Name": "Test",
+					"Summary": "Test",
+					"Notes": "Test"
+	        	},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(201);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("success");
+		});
+
+		test('when name is not provided', async () => {
+			const response = await campaign.Create({
+				payload: {
+					"Summary": "Test"
+				},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+	    	expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+	    	expect(response['messages'][0]).toBe("\"Name\" is required");
+		});
+
+		test('when summary is not provided', async () => {
+			const response = await campaign.Create({
+				payload: {
+					"Name": "Test"
+	        	},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("\"Summary\" is required");
+		});
+
+		test('when an empty string is provided as the name', async () => {
+			const response = await campaign.Create({
+				payload: {
+					"Name": "",
+					"Summary": "Test"
+	        	},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("\"Name\" is not allowed to be empty");
+		});
+
+		test('when credentials dont match the encounter', async () => {
+			const response = await campaign.Create({
+				payload: {
+					"Name": "Test",
+					"Summary": "Test",
+					"Encounters": [
+						{"Id": 2}
+	          		]
+				},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("\"Encounter Id\" 2 is invalid");
+		});
+
+		test('when invalid encounter is given', async () => {
+			const response = await campaign.Create({
+				payload: {
+					"Name": "Test",
+					"Summary": "Test",
+					"Encounters": [
+						{"Id": 3}
+	          		]
+				},
+				auth: {
+					credentials: {
+						id: 1
+					}
+	        	}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("\"Encounter Id\" 3 is invalid");
+		});
+
+		test('when credentials match the encounter', async () => {
+			const response = await campaign.Create({
+				payload: {
+					"Name": "Test",
+					"Summary": "Test",
+	        		"Encounters": [
+						{"Id": 2}
+					]
+				},
+				auth: {
+					credentials: {
+						id: 2
+	          		}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(201);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("success");
+		});
 	});
 });
-
 /*
 
 When all info provided correctly
