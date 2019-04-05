e3ed9667d8707f16d15a389bf79fce62b921756c
crazyfox44 <mcdermott.j.david@gmail.com>
2019-04-05 00:12:09 -0700

    Minor Formating

--- frontend/src/renderer/components/platform/pages/view_catalog/Monster.tsx
+++ frontend/src/renderer/components/platform/pages/view_catalog/Monster.tsx
@@ -72,7 +72,7 @@ export class Monster extends React.Component<any, IMonsterState> {
 		request(options, (error:string, responce: any, body: IMonsterDeleteResponse) => {
 			this.getPaginatedMonsters(this.state.page);
 		})
-  }
+	}
 
 	updatePage(page: number) {
 		this.getPaginatedMonsters(page);
@@ -114,12 +114,12 @@ export class Monster extends React.Component<any, IMonsterState> {
 	render() {
 		return (
 			<div id="view-monsters-container" className="layout card-grid">
-					<Pagination getTotalPages={this.getTotalPages} onPageChange={this.updatePage} ></Pagination>
-					<Grid container spacing={40}>
-						{this.state.monstersInCurrentPage.map(monster => (
-							<Grid item key={monster.Id} sm={6} md={4} lg={3}>
-								<Card className="card">
-								  <CardMedia
+				<Pagination getTotalPages={this.getTotalPages} onPageChange={this.updatePage} ></Pagination>
+				<Grid container spacing={40}>
+					{this.state.monstersInCurrentPage.map(monster => (
+						<Grid item key={monster.Id} sm={6} md={4} lg={3}>
+							<Card className="card">
+								<CardMedia
 									className="card-media"
 									image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
 									title="Image title" />
