c640402944b8d5ad7775a47cc2583ef24f85eb65
LeonGrund <15150811+LeonGrund@users.noreply.github.com>
2019-04-05 01:56:17 -0500

    Updated routes for campaign

--- frontend/src/renderer/app.tsx
+++ frontend/src/renderer/app.tsx
@@ -6,7 +6,7 @@ import { Route, Switch } from 'react-router-dom';
 import { HomePage } from './components/Home';
 import { Platform } from './components/Platform';
 import { MonsterCreation } from './components/MonsterCreation';
-import { CampaignCreation } from './components/CampaignCreation';
+import { CampaignCreation, CampaignCRUDState } from './components/CampaignCreation';
 import { EncounterCreation } from './components/EncounterCreation';
 import { ViewCatalog } from './components/platform/pages/ViewCatalog';
 import { ViewGameComponents } from './components/platform/pages/ViewGameComponents';
@@ -34,7 +34,14 @@ export class App extends React.Component<IAppProps> {
 								<Route exact path="/" component={Platform} />
 								<Route path="/encounter_creation" component={EncounterCreation} />
 								<Route path="/monster_creation" component={MonsterCreation} />
-								<Route path="/campaign_creation" component={CampaignCreation} />
+								<Route path="/campaign/create" render={(props) => {
+									return (
+										<CampaignCreation Process={CampaignCRUDState.Create} />
+									);}} />
+								<Route path="/campaign/view/:Id" render={(props) => {
+									return (
+										<CampaignCreation Process={CampaignCRUDState.Read} Id={props.match.params.Id} />
+									);}} />
 								<Route path="/catalog" component={ViewCatalog} />
 								<Route path="/game_components" component={ViewGameComponents} />
 								<Route path="" component={Platform} />
