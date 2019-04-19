da6b688f3d8a8ec3b700f93e6eb66b5e4e357f40
LeonGrund <15150811+LeonGrund@users.noreply.github.com>
2019-04-19 11:35:12 -0500

    Test Campaign CRUD, needs test for CampaignEdit

--- frontend/test/CampaignCreation.test.tsx
+++ frontend/test/CampaignCreation.test.tsx
@@ -2,18 +2,16 @@ import * as React from 'react';
 import * as nock from 'nock';
 import { mount, ReactWrapper } from 'enzyme';
 import {CampaignCRUD, ICampaignCRUDState, ICampaignCRUDProps, CampaignCRUDState } from "../src/renderer/components/CampaignCreation";
-
 //import {API_URL} from '../src/config'
 import { CookieManager as CookieManagerMock } from "../src/__mocks__/cookie";
 import { CookieManager } from "../src/cookie";
 import { BrowserRouter } from 'react-router-dom';
-
 jest.mock('../src/cookie');
 
 ////// Happy Path //////
 
 
-describe('Monster CRUD', () => {
+describe('Campaign CRUD', () => {
 
 	let campaignCreationInstance: ReactWrapper<ICampaignCRUDProps, ICampaignCRUDState, CampaignCRUD>;
 
@@ -72,7 +70,7 @@ describe('Monster CRUD', () => {
         //         "Encounters": []
 		// 	})
 		// 	.reply(201, { status: 201, messages: ['success'] });
-		// 	campaignCreationInstance.instance().createCampaign({ preventDefault() {} } as React.FormEvent);
+		// 	campaignCreationInstance.instance().submitForm({ preventDefault() {} } as React.FormEvent);
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
@@ -94,7 +92,7 @@ describe('Monster CRUD', () => {
         //         "Notes": "no"
 		// 	})
 		// 	.reply(404);
-		// 	campaignCreationInstance.instance().createCampaign({ preventDefault() {} } as React.FormEvent);
+		// 	campaignCreationInstance.instance().submitForm({ preventDefault() {} } as React.FormEvent);
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
@@ -116,7 +114,7 @@ describe('Monster CRUD', () => {
         //         "Notes": "no"
 		// 	})
 		// 	.reply(200, { status: 400, messages: ["Invalid campaign object"]});
-		// 	campaignCreationInstance.instance().createCampaign({ preventDefault() {} } as React.FormEvent);
+		// 	campaignCreationInstance.instance().submitForm({ preventDefault() {} } as React.FormEvent);
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
@@ -138,7 +136,7 @@ describe('Monster CRUD', () => {
         //         "Notes": "no"
 		// 	})
 		// 	.reply(200, { status: 401 });
-		// 	campaignCreationInstance.instance().createCampaign({ preventDefault() {} } as React.FormEvent);
+		// 	campaignCreationInstance.instance().submitForm({ preventDefault() {} } as React.FormEvent);
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
@@ -160,7 +158,7 @@ describe('Monster CRUD', () => {
         //         "Notes": "no"
 		// 	})
 		// 	.reply(200, { status: 401 });
-		// 	campaignCreationInstance.instance().createCampaign({ preventDefault() {} } as React.FormEvent);
+		// 	campaignCreationInstance.instance().submitForm({ preventDefault() {} } as React.FormEvent);
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
@@ -181,7 +179,7 @@ describe('Monster CRUD', () => {
         //         "Notes": "no"
 		// 	})
 		// 	.reply(200, { status: 401 });
-		// 	campaignCreationInstance.instance().createCampaign({ preventDefault() {} } as React.FormEvent);
+		// 	campaignCreationInstance.instance().submitForm({ preventDefault() {} } as React.FormEvent);
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
@@ -202,7 +200,7 @@ describe('Monster CRUD', () => {
         //         "Notes": "no"
 		// 	})
 		// 	.reply(200, { status: 401 });
-		// 	campaignCreationInstance.instance().createCampaign({ preventDefault() {} } as React.FormEvent);
+		// 	campaignCreationInstance.instance().submitForm({ preventDefault() {} } as React.FormEvent);
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
@@ -223,75 +221,17 @@ describe('Monster CRUD', () => {
         //         "Notes": "no"
 		// 	})
 		// 	.reply(200, { status: 401 });
-		// 	campaignCreationInstance.instance().createCampaign({ preventDefault() {} } as React.FormEvent);
-		// 	campaignCreationInstance.find('select#Race').simulate('change', { target: { value: "Devil" } })
-		// 	campaignCreationInstance.find('select#Environment').simulate('change', { target: { value: "Underdark" } })
-		// 	campaignCreationInstance.find('select#Alignment').simulate('change', { target: { value: "AnyGoodAlignment" } })
-		// 	campaignCreationInstance.find('Input#DamageVulnerabilities').simulate('change', { target: { value: 'Everything' } })
-		// 	campaignCreationInstance.find('Input#DamageResistances').simulate('change', { target: { value: 'None at all' } })
-		// 	campaignCreationInstance.find('Input#DamageImmunities').simulate('change', { target: { value: 'Nada' } })
-		// 	campaignCreationInstance.find('Input#ConditionImmunities').simulate('change', { target: { value: 'Nothing' } })
-		// 	campaignCreationInstance.find('Input#ArmorClass').simulate('change', { target: { value: 15 } })
-		// 	campaignCreationInstance.find('Input#HitPoints').simulate('change', { target: { value: 40 } })
-		// 	campaignCreationInstance.find('Input#HitPointDistribution').simulate('change', { target: { value: '9d5-5' } })
-		// 	campaignCreationInstance.find('Input#SpeedLand').simulate('change', { target: { value: 25 } })
-		// 	campaignCreationInstance.find('Input#SpeedSwim').simulate('change', { target: { value: 15 } })
-		// 	campaignCreationInstance.find('Input#AbilityScoresStrength').simulate('change', { target: { value: 17 } })
-		// 	campaignCreationInstance.find('Input#AbilityScoresDexterity').simulate('change', { target: { value: 15 } })
-		// 	campaignCreationInstance.find('Input#AbilityScoresConstitution').simulate('change', { target: { value: 13 } })
-		// 	campaignCreationInstance.find('Input#AbilityScoresIntelligence').simulate('change', { target: { value: 12 } })
-		// 	campaignCreationInstance.find('Input#AbilityScoresWisdom').simulate('change', { target: { value: 16 } })
-		// 	campaignCreationInstance.find('Input#AbilityScoresCharisma').simulate('change', { target: { value: 15 } })
-		// 	campaignCreationInstance.find('Input#SavingThrowsStrength').simulate('change', { target: { value: -3 } })
-		// 	campaignCreationInstance.find('Input#SavingThrowsDexterity').simulate('change', { target: { value: 0 } })
-		// 	campaignCreationInstance.find('Input#SavingThrowsConstitution').simulate('change', { target: { value: -1 } })
-		// 	campaignCreationInstance.find('Input#SavingThrowsIntelligence').simulate('change', { target: { value: -2 } })
-		// 	campaignCreationInstance.find('Input#SavingThrowsWisdom').simulate('change', { target: { value: 8 } })
-		// 	campaignCreationInstance.find('Input#SavingThrowsCharisma').simulate('change', { target: { value: 9 } })
-		// 	campaignCreationInstance.find('Input#Athletics').simulate('change', { target: { value: 9 } })
-		// 	campaignCreationInstance.find('Input#Acrobatics').simulate('change', { target: { value: 10 } })
-		// 	campaignCreationInstance.find('Input[id="Sleight of Hand"]').simulate('change', { target: { value: 9 } })
-		// 	campaignCreationInstance.find('Input#Stealth').simulate('change', { target: { value: 8 } })
-		// 	campaignCreationInstance.find('Input#Arcana').simulate('change', { target: { value: 7 } })
-		// 	campaignCreationInstance.find('Input#History').simulate('change', { target: { value: 7 } })
-		// 	campaignCreationInstance.find('Input#Investigation').simulate('change', { target: { value: 6 } })
-		// 	campaignCreationInstance.find('Input#Nature').simulate('change', { target: { value: 7 } })
-		// 	campaignCreationInstance.find('Input#Religion').simulate('change', { target: { value: 8 } })
-		// 	campaignCreationInstance.find('Input[id="Animal Handling"]').simulate('change', { target: { value: 9 } })
-		// 	campaignCreationInstance.find('Input#Insight').simulate('change', { target: { value: 10 } })
-		// 	campaignCreationInstance.find('Input#Medicine').simulate('change', { target: { value: 12 } })
-		// 	campaignCreationInstance.find('Input#Perception').simulate('change', { target: { value: 15 } })
-		// 	campaignCreationInstance.find('Input#Survival').simulate('change', { target: { value: 11 } })
-		// 	campaignCreationInstance.find('Input#Deception').simulate('change', { target: { value: 10 } })
-		// 	campaignCreationInstance.find('Input#Intimidation').simulate('change', { target: { value: 9 } })
-		// 	campaignCreationInstance.find('Input#Performance').simulate('change', { target: { value: 7 } })
-		// 	campaignCreationInstance.find('Input#Persuasion').simulate('change', { target: { value: 4 } })
-		// 	campaignCreationInstance.find('Input#Blind').simulate('change', { target: { value: 30 } })
-		// 	campaignCreationInstance.find('Input#Blindsight').simulate('change', { target: { value: 30 } })
-		// 	campaignCreationInstance.find('Input#Darkvision').simulate('change', { target: { value: 10} })
-		// 	campaignCreationInstance.find('Input#Tremorsense').simulate('change', { target: { value: 15 } })
-		// 	campaignCreationInstance.find('Input#Truesight').simulate('change', { target: { value: 60 } })
-		// 	campaignCreationInstance.find('Input[id="Passive Perception"]').simulate('change', { target: { value: 13 } })
-		// 	campaignCreationInstance.find('Input[id="Passive Investigation"]').simulate('change', { target: { value: 14 } })
-		// 	campaignCreationInstance.find('Input[id="Passive Insight"]').simulate('change', { target: { value: 16 } })
-		// 	campaignCreationInstance.find('Input#Languages').simulate('change', { target: { value: 'Common and Draconic' } })
-		// 	campaignCreationInstance.find('Input#ChallengeRating').simulate('change', { target: { value: 2.5 } })
-		// 	campaignCreationInstance.find('Input#ExperiencePoints').simulate('change', { target: { value: 190 } })
+		// 	campaignCreationInstance.instance().submitForm({ preventDefault() {} } as React.FormEvent);
+			
 		// 	//console.log(campaignCreationInstance.state())
 		// 	expect(campaignCreationInstance.state()).toEqual({
-		// 					Id: undefined,
+		// 		Id: undefined,
 		// 		Name: 'Hello',
 		// 		NameError: undefined,
-		// 		ChallengeRating: 2.5,
-		// 		ChallengeRatingError: undefined,
-		// 		ExperiencePoints: 190,
-		// 		Monster: {
-		// 			Name: '',
-		// 			AbilityScores: {},
-		// 			SavingThrows: {},
-		// 			Senses: {},
-		// 			Skills: {}
-		// 		},
+		// 		Summary: "Something in here.",
+		// 		SummaryError: undefined,
+		// 		Notes: "no",
+		// 		NotesError: undefined,
 		// 		submitted: false,
 		// 		modal: {
 		// 			open: false,
@@ -305,12 +245,12 @@ describe('Monster CRUD', () => {
         //         "Notes": "no"
 		// 	})
 		// 	.reply(201, { status: 201, message: 'success' });
-		// 	campaignCreationInstance.instance().createCampaign({ preventDefault() {} } as React.FormEvent);
+		// 	campaignCreationInstance.instance().submitForm({ preventDefault() {} } as React.FormEvent);
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	await new Promise(resolve => setImmediate(resolve));
 		// 	campaignCreationInstance.update();
-		// 	expect(campaignCreationInstance.find('#ModalMessage').text()).toEqual("Monster successfully created.");
+		// 	expect(campaignCreationInstance.find('#ModalMessage').text()).toEqual("Campaign successfully created.");
 		// 	expect(campaignCreationInstance.find('Modal#CampaignCRUDModal').prop('isActive')).toEqual(true);
 		// 	expect(nock.isDone()).toEqual(true);
 		// 	done();
---
+++ frontend/test/CampaignView.test.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import * as nock from 'nock';
+import { mount, ReactWrapper } from 'enzyme';
+import { CampaignCRUD, ICampaignCRUDState, ICampaignCRUDProps, CampaignCRUDState } from "../src/renderer/components/CampaignCreation";
+import {API_URL} from '../src/config'
+import { CookieManager as CookieManagerMock } from "../src/__mocks__/cookie";
+import { CookieManager } from "../src/cookie";
+
+jest.mock('../src/cookie');
+
+////// Happy Path //////
+
+describe('Campaign CRUD', () => {
+
+	let campaignCRUDInstance: ReactWrapper<ICampaignCRUDProps, ICampaignCRUDState, CampaignCRUD>;
+
+	const basicResponse = {
+		status: 201,
+		messages: ['success'],
+		content: { Name: 'Basic Campaign' }
+	}
+
+	describe('Read should be slightly different', () => {
+		beforeEach(async (done) => {
+			nock.disableNetConnect();
+			CookieManagerMock.SetStringCookie("session_token", "testToken");
+			// bind the normal user token function to the mock.
+			CookieManager.UserToken = CookieManagerMock.UserToken.bind(CookieManager);
+			CookieManager.RemoveCookie = CookieManagerMock.RemoveCookie.bind(CookieManager);
+			CookieManager.SetStringCookie = CookieManagerMock.SetStringCookie.bind(CookieManager);
+			nock(API_URL)
+			.get('/campaign/0')
+			.reply(200, basicResponse);
+			campaignCRUDInstance = mount<CampaignCRUD, ICampaignCRUDProps, ICampaignCRUDState>(<CampaignCRUD Process={CampaignCRUDState.Read} Id={0} />);
+			// THREE IS REQUIRED,SOMETHING TO DO WITH NESTING PROMISES
+			await new Promise(resolve => setImmediate(resolve));
+			await new Promise(resolve => setImmediate(resolve));
+			await new Promise(resolve => setImmediate(resolve));
+			// expect the CampaignCRUD to request the mosnter from the database.
+			expect(nock.isDone()).toEqual(true);
+			done();
+		});
+
+		afterEach( () => {
+			nock.cleanAll();
+		});
+
+		it('renders without crashing', () => {
+			expect(campaignCRUDInstance).toBeDefined();
+			expect(campaignCRUDInstance.state().Campaign.Name).toEqual('Basic Campaign');
+		});
+
+		it('should not render the submit button', () => {
+			expect(campaignCRUDInstance.find('Button#SubmitButton')).toHaveLength(0);
+		});
+	});
+})
