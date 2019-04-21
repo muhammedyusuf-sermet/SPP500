d5805345b4ceae1784ffb3e8d9c34849fdeb208e
Muhammed Ali Sit <masit@outlook.com>
2019-04-21 13:51:06 -0500

    create character pseudo tests

---
+++ backend/character.test.ts
@@ -0,0 +1,21 @@
+// create character pseudo tests
+// when all fields are properly given
+// create the character
+
+// when Name is not given
+// raise an error
+
+// when Race is not valid
+// raise an error
+
+// when Class is not valid
+// raise an error
+
+// when Level is not number
+// raise an error
+
+// when MaxHealth is not number
+// raise an error
+
+// when ArmorClass is not number
+// raise an error
