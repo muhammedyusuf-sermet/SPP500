4c0dcc0b87308bb0c6082ac46da94ed8e9c21037
crazyfox44 <mcdermott.j.david@gmail.com>
2019-04-21 11:54:57 -0700

    Consistent Id values

--- backend/entity/Character.ts
+++ backend/entity/Character.ts
@@ -6,6 +6,7 @@ import { User, IUserData } from "./User";
 export interface ICharacterData {
     Creator: IUserData;
 
+    Id: number;
     Name: string,
     Level: number,
     Race: CharacterRace,
