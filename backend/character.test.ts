96febc29c4957311c2ad0b92859eea4c661a7dd0
Muhammed Ali Sit <masit@outlook.com>
2019-04-21 14:32:07 -0500

    character creation backend tests

--- backend/character.test.ts
+++ backend/character.test.ts
@@ -1,21 +1,189 @@
-// create character pseudo tests
-// when all fields are properly given
-// create the character
+import {User} from "./entity/User"
+import {Campaign} from "./entity/Campaign"
 
-// when Name is not given
-// raise an error
+import {CharacterFactory} from "./character";
 
-// when Race is not valid
-// raise an error
+jest.mock("./entity/Campaign");
+jest.mock("./entity/Character");
+jest.mock("./entity/User");
 
-// when Class is not valid
-// raise an error
 
-// when Level is not number
-// raise an error
+describe('character tests', async () => {
+	beforeAll( async () => {
+		let user = new User();
+		user.Name = "John Doe";
+		user.Id = 1;
+		await user.save();
+	
+		let campaign = new Campaign();
+		campaign.Name = "Test1";
+		campaign.Id = 1;
+		campaign.Creator = user;
+		await campaign.save();
 
-// when MaxHealth is not number
-// raise an error
+		const campaign2 = new Campaign();
+		campaign2.Name = "Test2";
+		campaign2.Id = 2;
+		campaign2.Creator = user;
+		await campaign2.save();
 
-// when ArmorClass is not number
-// raise an error
+		const user2 = new User();
+		user2.Name = "Other John Doe";
+		user2.Id = 2;
+		await user2.save();
+
+		let campaign3 = new Campaign();
+		campaign3.Name = "Test3";
+		campaign3.Id = 3;
+		campaign3.Creator = user2;
+		await campaign3.save();
+    
+    	
+	});
+
+	describe('character creation tests', async () => {
+
+		var character = new CharacterFactory();
+
+		test('when proper data is given', async () => {
+			const response = await character.Create({
+				payload: {
+					"Name": "Test",
+					"Campaigns": [
+						{ "Id": 1 }
+					]
+	        	},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(201);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("success");
+		});
+
+		test('when no campaign is provided', async () => {
+			const response = await character.Create({
+				payload: {
+					"Name": "Test"
+	        	},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(201);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("success");
+		});
+
+		test('when name is not provided', async () => {
+			const response = await character.Create({
+				payload: {
+					"Notes": "Test"
+				},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("\"Name\" is required");
+		});
+
+
+		test('when an empty string is provided as the name', async () => {
+			const response = await character.Create({
+				payload: {
+					"Name": ""
+				},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("\"Name\" is not allowed to be empty");
+		});
+
+		test('when credentials dont match the campaign', async () => {
+			const response = await character.Create({
+				payload: {
+					"Name": "Test",
+					"Campaigns": [
+						{"Id": 3}
+					]
+				},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("\"Campaign Id\" 3 is invalid");
+		});
+
+		test('when invalid campaign is given', async () => {
+			const response = await character.Create({
+				payload: {
+					"Name": "Test",
+					"Campaigns": [
+						{"Id": 4}
+					]
+				},
+				auth: {
+					credentials: {
+						id: 1
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(400);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("\"Campaign Id\" 4 is invalid");
+		});
+
+		test('when credentials match the campaign', async () => {
+			const response = await character.Create({
+				payload: {
+					"Name": "Test",
+					"Campaigns": [
+						{"Id": 3}
+					]
+				},
+				auth: {
+					credentials: {
+						id: 2
+					}
+				}
+			});
+
+			expect.assertions(3);
+			expect(response['status']).toBe(201);
+			expect(response['messages'].length).toBe(1)
+			expect(response['messages'][0]).toBe("success");
+		});
+	});
+
+});
+
