dd00325ea022aa2da3fa9421a097376740f65aa6
crazyfox44 <mcdermott.j.david@gmail.com>
2019-04-21 23:52:29 -0700

    The delet path is a DELETE

--- frontend/src/renderer/components/platform/pages/view_game_components/EncounterList.tsx
+++ frontend/src/renderer/components/platform/pages/view_game_components/EncounterList.tsx
@@ -30,10 +30,7 @@ interface IEncounterDeleteResponse {
 }
 
 export interface IEncounterListState {
-	viewEncounter: boolean,
-	editEncounter: boolean,
-	selectedEncounter: EncounterInterface.IEncounterState,
-
+	page: number;
 	pageSize: number,
 	totalEncounters: number,
 	encountersInCurrentPage: EncounterInterface.IEncounterState[],
@@ -43,10 +40,7 @@ export class EncounterList extends React.Component<any, IEncounterListState> {
 	constructor(props: any) {
 		super(props);
 		this.state = {
-			viewEncounter: false,
-			editEncounter: false,
-			selectedEncounter: {} as EncounterInterface.IEncounterState,
-
+			page: 0,
 			pageSize: 12,
 			totalEncounters: 0,
 			encountersInCurrentPage: [] as EncounterInterface.IEncounterState[],
@@ -60,9 +54,7 @@ export class EncounterList extends React.Component<any, IEncounterListState> {
 	}
 
 	resetState() {
-		this.setState({ selectedEncounter: {} as EncounterInterface.IEncounterState});
-		this.setState({ viewEncounter: false});
-		this.setState({ editEncounter: false});
+		this.setState({ page: 0});
 		this.getPaginatedEncounters(0);
 	}
 
@@ -74,36 +66,18 @@ export class EncounterList extends React.Component<any, IEncounterListState> {
 		return Math.ceil(this.state.totalEncounters / this.state.pageSize)-1;
 	}
 
-	view = (encounter: EncounterInterface.IEncounterState) => {
-		this.setState({ selectedEncounter: encounter});
-		this.setState({ viewEncounter: true});
-	}
-
-	edit = (encounter: EncounterInterface.IEncounterState) => {
-		this.setState({ selectedEncounter: encounter});
-		this.setState({ editEncounter: true});
-	}
-
 	deleteEncounter = (event: React.MouseEvent<HTMLButtonElement>) => {
-		event.preventDefault();
-
-		var options = { method: 'POST',
-			url: API_URL + '/encounter/delete',
-			timeout: 2000,
+		var options = { method: 'DELETE',
+			url: API_URL + '/encounter/' + event.currentTarget.value,
 			headers:
 			{
 				'Cache-Control': 'no-cache',
-				'Content-Type': 'application/json' ,
 				'Authorization': CookieManager.UserToken('session_token')
-			},
-			body:
-			{
-				Id: event.currentTarget.value,
-			},
-			json: true
+			}
 		};
+
 		request(options, (error: string, response: any, body: IEncounterDeleteResponse) => {
-			this.getPaginatedEncounters(0);
+			this.getPaginatedEncounters(this.state.page);
 		});
 	}
 
@@ -137,54 +111,46 @@ export class EncounterList extends React.Component<any, IEncounterListState> {
 	}
 
 	render() {
-		if(!this.state.viewEncounter && !this.state.editEncounter){
-			return (
-				<div id="view-encounters-container" className= "layout card-grid">
-					<Pagination getTotalPages={this.getTotalPages} onPageChange={this.updatePage} ></Pagination>
-					<Grid container spacing={40}>
-						{this.state.encountersInCurrentPage.map(encounter => (
-							<Grid item key={encounter.Id} sm={6} md={4} lg={3}>
-								<Card className="card">
-									<CardMedia
-										className="card-media"
-										image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-										title="Image title"
-									/>
-									<CardContent className="card-content">
-										<Typography gutterBottom variant="h5" component="h2">
-											{encounter.Name} {encounter.Id}
-										</Typography>
-										<Typography>
-											{encounter.Description}.
-										</Typography>
-									</CardContent>
-									<CardActions>
-										<Link to={ "/encounter/view/"+encounter.Id }>
-											View
-										</Link>
-									  <Link to={ "/encounter/run/"+encounter.Id }>
-										  Run
-									  </Link>
-										<Link to={ "/encounter/edit/"+encounter.Id }>
-											Edit
-										</Link>
-										<Button variant="contained" color="secondary" value={encounter.Id} onClick={this.deleteEncounter} >
-											Delete
-											<DeleteIcon/>
-										</Button>
-									</CardActions>
-								</Card>
-							</Grid>
-						))}
-					</Grid>
-				</div>
-			)
-		}
-		// else if(this.state.viewEncounter){
-		// 	return (<EncounterDetails encounter={this.state.selectedEncounter} resetParentState={this.resetState}/>);
-		// }
-		else{
-			return (<div>There has been an error on our side. Please check back later!</div>);
-		}
+		return (
+			<div id="view-encounters-container" className= "layout card-grid">
+				<Pagination getTotalPages={this.getTotalPages} onPageChange={this.updatePage} ></Pagination>
+				<Grid container spacing={40}>
+					{this.state.encountersInCurrentPage.map(encounter => (
+						<Grid item key={encounter.Id} sm={6} md={4} lg={3}>
+							<Card className="card">
+								<CardMedia
+									className="card-media"
+									image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
+									title="Image title"
+								/>
+								<CardContent className="card-content">
+									<Typography gutterBottom variant="h5" component="h2">
+										{encounter.Name} {encounter.Id}
+									</Typography>
+									<Typography>
+										{encounter.Description}.
+									</Typography>
+								</CardContent>
+								<CardActions>
+									<Link to={ "/encounter/view/"+encounter.Id }>
+										View
+									</Link>
+								  <Link to={ "/encounter/run/"+encounter.Id }>
+									  Run
+								  </Link>
+									<Link to={ "/encounter/edit/"+encounter.Id }>
+										Edit
+									</Link>
+									<Button variant="contained" color="secondary" value={encounter.Id} onClick={this.deleteEncounter} >
+										Delete
+										<DeleteIcon/>
+									</Button>
+								</CardActions>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
+			</div>
+		);
 	}
 }
