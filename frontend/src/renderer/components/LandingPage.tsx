f2a9859eaa6bebf933513d2b0f8f168f5f50641c
David McDermott <mcdermott.j.david@gmail.com>
2019-05-06 14:34:22 -0500

    Update LandingPage.tsx
    doc -> docs

--- frontend/src/renderer/components/LandingPage.tsx
+++ frontend/src/renderer/components/LandingPage.tsx
@@ -4,7 +4,7 @@ import '../css/landing_page.css';
 export const LandingPage: React.StatelessComponent<{}> = () => {
 	return (
 		<div className="landing-container">
-			<img src={require('../../../../doc/art/DM-Tools-Logo.png')} className="logo"></img>
+			<img src={require('../../../../docs/art/DM-Tools-Logo.png')} className="logo"></img>
 			<h3>One-stop platform for Dungeon Masters!</h3>
 		</div>
 	);
